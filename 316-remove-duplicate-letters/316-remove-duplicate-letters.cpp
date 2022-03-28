4bc1a404c8a9e3e3f865d5a2f9857fa0d85aaaac
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-03-28 21:32:02 +0530

    Update 316-remove-duplicate-letters.cpp

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -19,6 +19,12 @@ public:
             
             if(cc[s[i]-'a']==0)
             {
+                // dekh bhai agr stack ka size bda hoga '0' se tho koi element hai yani andr
+                // hume ye check krna hae ki jo element ham insert s[i] krne vale hein kia uske andr jo elemet hai vo
+                // bda hai ya chota agr chota tb while ni chlega and sahi ho jaega
+                // vrna chlega while and ye bi check hoga ki
+                // kahi element unique tho nahi hai kuki future mae exist krna chye agr uski prefernce badani hai 
+                // jese ki agr e occurs 1 time then if we will decrement it thgis might not help
                 while(stk.size()>0 && stk.top()>s[i] && check[stk.top()-'a']>0 )
                 {
                     // STORING THE ELEMENT THAT WE NEED TO POP OUT AND STORING IT INTO TEMP
