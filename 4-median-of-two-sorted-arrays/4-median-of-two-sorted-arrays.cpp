9495ccda352da8f64483a96534ebe95d367d9bc8
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-03-31 22:50:34 +0530

    Time: 62 ms (38.00%), Space: 93 MB (8.66%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,36 +1,42 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums2.size() < nums1.size()) return findMedianSortedArrays(nums2, nums1);
-        int n1 = nums1.size();
-        int n2 = nums2.size(); 
-        int low = 0, high = n1;
-        
-        while(low <= high) {
-            int cut1 = (low+high) >> 1;
-            int cut2 = (n1 + n2 + 1) / 2 - cut1; 
-            
-        
-            int left1 = cut1 == 0 ? INT_MIN : nums1[cut1-1];
-            int left2 = cut2 == 0 ? INT_MIN : nums2[cut2-1]; 
-            
-            int right1 = cut1 == n1 ? INT_MAX : nums1[cut1];
-            int right2 = cut2 == n2 ? INT_MAX : nums2[cut2]; 
-            
-            
-            if(left1 <= right2 && left2 <= right1) {
-                if( (n1 + n2) % 2 == 0 ) 
-                    return (max(left1, left2) + min(right1, right2)) / 2.0; 
-                else 
-                    return max(left1, left2); 
-            }
-            else if(left1 > right2) {
-                high = cut1 - 1; 
-            }
-            else {
-                low = cut1 + 1; 
-            }
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
+    {
+        double sol=1;
+       // CASE1:- 1  2  3  3  4  6  7  10  12  15
+       // INDEX:- 0  1  2  3  4  5  6  7   8   9  
+        multiset<int> m;
+        for(auto i:nums1)
+        {
+            m.insert(i);
         }
-        return 0.0; 
+        for(auto i:nums2)
+        {
+            m.insert(i);
+        }
+        
+        int a=m.size(); // SIZE OF THE MULTISET
+        // INITIALISING THE ITERATOR TO THE START OF THE MAP
+        auto itr=m.begin();
+        auto itr1=m.begin();
+        
+        advance(itr,a/2);
+        if(a%2!=0)
+        {
+            sol=sol*(*itr);
+        }
+        else
+        {
+            advance(itr1,a/2-1);
+                sol= (*itr+*itr1)/2.0;
+        }
+        
+        
+        return sol;
+    
+    
+    
+    
+    
     }
 };
