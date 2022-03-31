e7d4a96c1a8439ae8f557612ea6383e54fcba973
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-03-31 21:44:09 +0530

    Time: 128 ms (5.04%), Space: 7.7 MB (23.95%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -2,11 +2,17 @@ class Solution {
 public:
     bool isPalindrome(string s) 
     {
-        vector<char>a;
+        string a;
         
         int i,j;
         i=0,j=s.length();
         //s.erase(remove(s.begin(),s.end(),' '),s.end());
+        for(int k=0;k<j;k++)
+        {
+            if(s[k]==' ')
+                s[k]='\0';
+        }
+        for(auto i:s) cout<<i;
         cout<<s<<endl;
         transform(s.begin(),s.end(),s.begin(),::tolower);
         cout<<s<<endl;
