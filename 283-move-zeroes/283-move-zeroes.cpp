a8fc51a1125e4ee5b7fe0561b5dbf5fd0ae29cf9
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-04-04 22:01:50 +0530

    Time: 24 ms (81.25%), Space: 19.3 MB (13.92%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -5,36 +5,22 @@ public:
         // QUICK SORT APPROACH
         // EXCEPT ZERO PUT ALL THE ELEMENTS IN THE SAME ORDER
        // [0,1,0,3,12]------> 1,3,12,0,0
-       /* int n=nums.size();
+        int n=nums.size();
         int k=0,z=0;
      for(int i=0;i<n;i++)
         {
             if(nums[i]!=0)
             {
                 nums[k++]=nums[i];
-                cout<<nums[k]<<"...";   
             }
         }
-        for(int i=k;i<n;i++)
+        for(;k<n;k++)
         {
-            nums[i]=0;
+            nums[k]=0;
         }
      }  
         //return nums;
         
-        */
-        
-        int j = 0;
-        // move all the nonzero elements advance
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != 0) {
-                nums[j++] = nums[i];
-            }
-        }
-        for (;j < nums.size(); j++) {
-            nums[j] = 0;
-        }
-    }
         
     
 };
