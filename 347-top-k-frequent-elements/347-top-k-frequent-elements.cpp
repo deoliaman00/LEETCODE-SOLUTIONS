3bf3f18ebbdb2d618b834adc95b2ebe43fbd93cb
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-04-09 23:08:54 +0530

    Time: 20 ms (61.60%), Space: 13.7 MB (71.77%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) 
+    {
+      priority_queue<pair<int,int>  ,vector<pair<int,int>> , greater<pair<int,int>>> minheap;
+        vector<int> ans;
+    unordered_map<int,int> hashmp;
+        for(auto i:nums)
+        {
+            hashmp[i]++;
+            
+        }
+        
+        for(auto &i:hashmp)
+        {
+            minheap.push({i.second,i.first});
+            if(minheap.size()>k)
+                minheap.pop();
+            
+        }
+        
+        while(k--)
+        {
+            ans.push_back(minheap.top().second);
+            minheap.pop();
+                
+        
+        }
+        
+        
+        return ans;
+    }
+};
