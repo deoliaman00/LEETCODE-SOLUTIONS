3210262324f2b854366cce490e634d5ec3cb9cb8
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-04-09 08:52:18 +0530

    Time: 4 ms (56.79%), Space: 6.2 MB (82.79%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -43,7 +43,7 @@ public:
             int sum = carry;
             if(i >= 0) sum += a[i--] - '0';
             if(j >= 0) sum += b[j--] - '0';
-            carry = sum > 1 ? 1 : 0;
+            carry = sum/2;
             result += to_string(sum%2);
         }
         if(carry) result += to_string(carry);
