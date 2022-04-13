2148876101ddcde918232b43f6ab4166820de138
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-04-13 09:18:23 +0530

    Time: 2 ms (46.10%), Space: 6.9 MB (58.40%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -1,40 +1,133 @@
 class Solution {
 public:
+    int count(vector<vector<int>>& board,int i,int j)
+    {
+        int r=board.size();
+        int c=board[0].size();
+        int livec=0;
+        //.... for horizontal 1
+        if(j+1<c)
+        {
+            if(board[i][j+1]==1 || board[i][j+1]==3)
+            {
+                livec++;
+            }
+        }
+        //.... for horizontal back 1
+        if(j-1>=0)
+        {
+            if(board[i][j-1]==1 || board[i][j-1]==3)
+            {
+                livec++;
+            }
+        }
+        //.... for vertical 1
+        if(i+1<r)
+        {
+            if(board[i+1][j]==1 || board[i+1][j]==3)
+            {
+                livec++;
+            }
+        }
+        //.... for vertical down 1
+        if(i-1>=0)
+        {
+            if(board[i-1][j]==1 || board[i-1][j]==3)
+            {
+                livec++;
+            }
+        }
         
-    bool isValidNeighbor(int x, int y, vector<vector<int>>& board) {
-        return (x >= 0 && x < board.size() && y >= 0 && y < board[0].size());
+        //...... for diagonals
+        
+        if(i+1<r && j-1>=0)
+        {
+            if(board[i+1][j-1]==1 || board[i+1][j-1]==3)
+            {
+                livec++;
+            }
+        }
+        if(i+1<r && j+1<c)
+        {
+            if(board[i+1][j+1]==1 || board[i+1][j+1]==3)
+            {
+                livec++;
+            }
+        }
+        if(i-1>=0 && j-1>=0)
+        {
+            if(board[i-1][j-1]==1 || board[i-1][j-1]==3)
+            {
+                livec++;
+            }
+        }
+        if(i-1>=0 && j+1<c)
+        {
+            if(board[i-1][j+1]==1 || board[i-1][j+1]==3)
+            {
+                livec++;
+            }
+        }
+        
+        return livec;
+         
     }
     
-    void gameOfLife(vector<vector<int>>& board) { 
-        vector<int> dx = {0, 0, 1, 1, 1, -1, -1, -1}; //8 coordinated for neighbours
-        vector<int> dy = {1, -1, 1, -1, 0, 0, 1, -1};
+    
+    
+    
+    void gameOfLife(vector<vector<int>>& board)
+    {
+        //............. board = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]]
+       //...............Output: [[0,0,0],[1,0,1],[0,1,1],[0,1,0]]
+        int r1=board.size(); // r1=4
+        int c1=board[0].size(); // c1=3
         
-        for (int row = 0; row < board.size(); row++) {
-            for (int col = 0; col < board[0].size(); col++) {
+        int tac=0;
+        
+        for(int i=0;i<r1;i++)
+        {
+            for(int j=0;j<c1;j++)
+            {
+                tac=count(board,i,j);
+                //total_active_cells= count(board,i,j);
                 
-                int count_live_neighbors = 0;
-                
-                for (int i = 0; i < 8; i++) {
-                    int curr_x = row + dx[i], curr_y = col +dy[i];
-                    if (isValidNeighbor(curr_x, curr_y, board) && abs(board[curr_x][curr_y]) == 1)
-                        count_live_neighbors++;
+                if(board[i][j]==1)
+                {
+                    if(tac<2 || tac>3)
+                    {
+                        board[i][j]=3;
+                    }  
                 }
-                
-                if (board[row][col] == 1 && (count_live_neighbors < 2 || count_live_neighbors > 3))
-                    board[row][col] = -1;
-                
-                if (board[row][col] == 0 && count_live_neighbors == 3)
-                    board[row][col] = 2;
+                else if(board[i][j]==0)
+                {
+                    if(tac==3)
+                    {
+                        board[i][j]=2;
+                    }
+                   
+                }
             }
         }
         
-        for (int row = 0; row < board.size(); row++) {
-            for (int col = 0; col < board[0].size(); col++) {
-                if (board[row][col] >= 1)
-                    board[row][col] = 1;
-                else
-                    board[row][col] = 0;
+        
+        for(int i=0;i<r1;i++)
+        {
+            for(int j=0;j<c1;j++)
+            {
+                if(board[i][j]==3)
+                {
+                    board[i][j]=0;
+                }
+                else if(board[i][j]==2)
+                {
+                   board[i][j]=1;
+                }
             }
         }
+        
+        
+        
+        
     }
 };
