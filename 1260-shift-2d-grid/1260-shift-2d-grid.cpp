6e5e392e3a46e723d7a85d118f652309a79bf8f8
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-04-13 06:10:23 +0530

    Time: 31 ms (67.65%), Space: 14.1 MB (35.96%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) 
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k)
     {
-        const int a = grid.size();
-        const int b = grid[0].size();
-        vector<vector<int>> ans(a, vector<int>(b, 0));
-        for (int i = 0; i < a * b; ++i) {
-            int ori_y = i / b;
-            int ori_x = i % b;
-            int new_y = (ori_y + (ori_x + k) / b) % a;
-            int new_x = (ori_x + k) % b;
-            
-            ans[new_y][new_x] = grid[ori_y][ori_x];
+        int n1=grid.size();
+        int n2=grid[0].size();
+        vector<vector<int>> shift(grid.size(),vector<int>(grid[0].size(),0));
+        
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<grid[i].size();j++)
+            {
+                shift[(i+(j+k)/grid[i].size())%grid.size()][(j+k)%grid[i].size()]= grid[i][j];
+            }
         }
-        return ans;
+        return shift;
         
     }
+        
+    
 };
