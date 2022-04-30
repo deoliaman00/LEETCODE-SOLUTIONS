b52fefb0c5d75d507715aac7559e30b9494299fa
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-04-30 06:16:06 +0530

    Time: 210 ms (40.36%), Space: 52.1 MB (90.27%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.cpp
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -1,54 +1,56 @@
 class MyHashMap {
-	vector<vector<pair<int, int>>> map;
-	const int size = 10000;
 public:
-	/** Initialize your data structure here. */
-	MyHashMap() {
-		map.resize(size);
-	}
-
-	/** value will always be non-negative. */
-	void put(int key, int value) {
-		int index = key % size;
-        vector<pair<int, int>> &row = map[index];
-        for(auto iter = row.begin(); iter != row.end(); iter++)
+    vector<list<pair<int,int>>>ans;
+    int size=10;
+    MyHashMap()
+    {
+        ans.resize(size);
+        
+    } 
+    int keygive(int n)
+    {
+        return n%size;
+    }
+    void put(int key, int value)
+    {
+     int f=keygive(key);
+        for(auto it=ans[f].begin();it!=ans[f].end();it++)
         {
-            if(iter->first == key)
+            if(it->first==key)
             {
-                iter->second = value;
+                it->second=value;
                 return;
             }
         }
-		row.push_back(make_pair(key, value));
-	}
-
-	/** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
-	int get(int key) {
-		int index = key % size;
-        vector<pair<int, int>> &row = map[index];
-		for (auto iter = row.begin(); iter != row.end(); iter++)
-		{
-			if (iter->first == key)
-			{
-				return iter->second;
-			}
-		}
-		return -1;
-	}
-
-	/** Removes the mapping of the specified value key if this map contains a mapping for the key */
-	void remove(int key) {
-		int index = key % size;
-        vector<pair<int, int>> &row = map[index];
-		for (auto iter = row.begin(); iter != row.end(); iter++)
-		{
-			if (iter->first == key)
-			{
-				row.erase(iter);
+        ans[f].push_back({key,value});    
+        
+    }
+    
+    int get(int key)
+    {
+        int f=keygive(key);
+        for(auto it=ans[f].begin();it!=ans[f].end();it++)
+        {
+            if(it->first==key)
+            {
+                return it->second;
+            }
+        }
+        return -1;
+    }    
+    void remove(int key)
+    {
+        
+      int f=keygive(key);
+        for(auto it=ans[f].begin();it!=ans[f].end();it++)
+        {
+            if(it->first==key)
+            {
+                ans[f].erase(it);
                 return;
-			}
-		}
-	}
+            }
+        }  
+    }
 };
 
 /**
