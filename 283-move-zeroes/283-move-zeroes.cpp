a12f396790667010c463d1ad401ac16e0acb349e
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-04 22:17:05 +0530

    Time: 36 ms (38.35%), Space: 19.2 MB (58.75%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -5,22 +5,31 @@ public:
         // QUICK SORT APPROACH
         // EXCEPT ZERO PUT ALL THE ELEMENTS IN THE SAME ORDER
        // [0,1,0,3,12]------> 1,3,12,0,0
+     //    int n=nums.size();
+     //    int k=0,z=0;
+     // for(int i=0;i<n;i++)
+     //    {
+     //        if(nums[i]!=0)
+     //        {
+     //            nums[k++]=nums[i];
+     //        }
+     //    }
+     //    for(int i=k;i<n;i++)
+     //    {
+     //        nums[i]=0;
+     //    }
+     // }  
+     //    //return nums;
         int n=nums.size();
-        int k=0,z=0;
-     for(int i=0;i<n;i++)
+        int swapper=0,cur=0;
+        for(;cur<n;cur++)
         {
-            if(nums[i]!=0)
-            {
-                nums[k++]=nums[i];
-            }
+           if(nums[cur]!=0)
+           {
+               swap(nums[cur],nums[swapper++]);
+           }
         }
-        for(int i=k;i<n;i++)
-        {
-            nums[i]=0;
-        }
-     }  
-        //return nums;
         
         
-    
+    }
 };
