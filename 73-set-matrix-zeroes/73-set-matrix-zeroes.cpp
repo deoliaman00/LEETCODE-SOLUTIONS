b04fc7796e73817aed9182a6cbf928d4208d9326
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-04 15:43:09 +0530

    Time: 16 ms (72.41%), Space: 13.2 MB (54.80%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -2,44 +2,63 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix)
     {
-        int rowSize=matrix.size();
-        int colSize=matrix[0].size();
-        vector<pair<int,int>>ans;
+       
+        // 0(n) space consumed
         
-        for(int i=0;i<rowSize;i++)
-        {
-            for(int j=0;j<colSize;j++)
-            {
-                if(matrix[i][j]==0)
-                {
-                    ans.push_back(make_pair(i,j));
-                }
-            }
-        }
+//         int rowSize=matrix.size();
+//         int colSize=matrix[0].size();
+//         vector<pair<int,int>>ans;
         
-        for(auto a:ans)
-        {
+//         for(int i=0;i<rowSize;i++)
+//         {
+//             for(int j=0;j<colSize;j++)
+//             {
+//                 if(matrix[i][j]==0)
+//                 {
+//                     ans.push_back(make_pair(i,j));
+//                 }
+//             }
+//         }
+        
+//         for(auto a:ans)
+//         {
             
-                cout<<a.first<<" "<<a.second<<" ";
+//                 cout<<a.first<<" "<<a.second<<" ";
         
-        }
+//         }
         
-    for(auto a:ans)
-    {
-        for(int i=0;i<rowSize;i++)
-        {
-            for(int j=0;j<colSize;j++)
-            {
-               if(i==a.first) // 1 1 
-               {                
-                matrix[i][j]=0;
-               }
-            else if(j==a.second)
-            {
-                matrix[i][j]=0;
-            }
-            }
-        }
+//     for(auto a:ans)
+//     {
+//         for(int i=0;i<rowSize;i++)
+//         {
+//             for(int j=0;j<colSize;j++)
+//             {
+//                if(i==a.first) // 1 1 
+//                {                
+//                 matrix[i][j]=0;
+//                }
+//             else if(j==a.second)
+//             {
+//                 matrix[i][j]=0;
+//             }
+//             }
+//         }
+//     }
+        
+             int col0 = 1, rows = matrix.size(), cols = matrix[0].size();
+
+    for (int i = 0; i < rows; i++) {
+        if (matrix[i][0] == 0) col0 = 0;
+        for (int j = 1; j < cols; j++)
+            if (matrix[i][j] == 0)
+                matrix[i][0] = matrix[0][j] = 0;
+    }
+
+    for (int i = rows - 1; i >= 0; i--) {
+        for (int j = cols - 1; j >= 1; j--)
+            if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                matrix[i][j] = 0;
+        if (col0 == 0) matrix[i][0] = 0;
     }
         
         
