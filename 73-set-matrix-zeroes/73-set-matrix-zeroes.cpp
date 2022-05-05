28307cfd8d1eae978fd68366f3e5318a1402c771
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-05 06:04:52 +0530

    Time: 13 ms (81.53%), Space: 13.3 MB (19.27%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix)
+    void setZeroes(vector<vector<int>>& nums)
     {
        
         // 0(n) space consumed
@@ -45,43 +45,28 @@ public:
 //         }
 //     }
         
-//              int rows=nums.size(),col=nums[0].size();
-//             int flag=1;
-//         for(int i=0;i<rows;i++)
-//         {
-//             if(nums[i][0]==0) flag=0;
-//             for(int j=1;j<col;j++)
-//             {
-//                 if(nums[i][j]==0)
-//                 {
-//                     nums[0][j]=0;
-//                     nums[i][0]=0;
-//                 }
-//             }
-//         }
+             int rows=nums.size(),col=nums[0].size();
+            int flag=1;
+        for(int i=0;i<rows;i++)
+        {
+            if(nums[i][0]==0) flag=0;
+            for(int j=1;j<col;j++)
+            {
+                if(nums[i][j]==0)
+                {
+                    nums[i][0]=nums[0][j]=0;
+                    
+                }
+            }
+        }
         
-//         for(int i=rows-1;i>=0;i--)
-//         {
-//             for(int j=col-1;j>=1;j--)
-//                if(nums[i][0]==0 || nums[0][j]==0)
-//                    nums[i][j]=0;
-//             if(col==0)  nums[i][0]=0;
-//         }
-         int col0 = 1, rows = matrix.size(), cols = matrix[0].size();
-
-    for (int i = 0; i < rows; i++) {
-        if (matrix[i][0] == 0) col0 = 0;
-        for (int j = 1; j < cols; j++)
-            if (matrix[i][j] == 0)
-                matrix[i][0] = matrix[0][j] = 0;
-    }
-
-    for (int i = rows - 1; i >= 0; i--) {
-        for (int j = cols - 1; j >= 1; j--)
-            if (matrix[i][0] == 0 || matrix[0][j] == 0)
-                matrix[i][j] = 0;
-        if (col0 == 0) matrix[i][0] = 0;
-    }
-        
+        for(int i=rows-1;i>=0;i--)
+        {
+            for(int j=col-1;j>=1;j--)
+               if(nums[i][0]==0 || nums[0][j]==0)
+                   nums[i][j]=0;
+            if(flag==0)  nums[i][0]=0;
+        }
+         
     }
 };
