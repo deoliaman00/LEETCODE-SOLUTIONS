7d12f75f51671775464dd7d5c6700f115e3148f5
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-05 12:16:06 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (58.19%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -1,54 +1,120 @@
+// class MyStack {
+// public:
+//     queue<int>q1;
+//         queue<int>q2;
+//     MyStack()
+//     {}
+    
+//     void push(int x) 
+//     {
+//         //.....Steps.....insert in q2....swap all ele in q2 of q1......swap all ele in q1 from q2.....
+//         q2.push(x);
+//         while(q1.empty()==0)
+//         {
+//             q2.push(q1.front());
+//             q1.pop();
+//         }
+        
+//          while(q2.empty()==0)
+//         {
+//             q1.push(q2.front());
+//             q2.pop();
+//         }
+        
+        
+//     }
+    
+//     int pop()
+//     {
+//         int a=q1.front();
+//         q1.pop();
+//         return a;
+        
+//     }
+    
+//     int top() 
+//     {
+//         int a=q1.front();
+//         return a;
+        
+//     }
+    
+//     bool empty()
+//     {
+//         if(q1.empty()==0)
+//         {
+//             return false;
+//         }
+//         else
+//         {
+//             return true;
+//         }
+        
+//     }
+// };
+
+// /**
+//  * Your MyStack object will be instantiated and called as such:
+//  * MyStack* obj = new MyStack();
+//  * obj->push(x);
+//  * int param_2 = obj->pop();
+//  * int param_3 = obj->top();
+//  * bool param_4 = obj->empty();
+//  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//........................using one queue..........
+
 class MyStack {
 public:
-    queue<int>q1;
-        queue<int>q2;
-    MyStack()
-    {}
+queue<int> q;
+	// Push element x onto stack.
+	
     
-    void push(int x) 
+    void push(int x)
     {
-        //.....Steps.....insert in q2....swap all ele in q2 of q1......swap all ele in q1 from q2.....
-        q2.push(x);
-        while(q1.empty()==0)
+		q.push(x);
+		for(int i=0;i<q.size()-1;++i)
         {
-            q2.push(q1.front());
-            q1.pop();
-        }
-        
-         while(q2.empty()==0)
-        {
-            q1.push(q2.front());
-            q2.pop();
-        }
-        
+			q.push(q.front());
+			q.pop();
+		}
+	}
+    
+    int pop() 
+    {
+        int a1=q.front();
+        q.pop();
+        return a1;
         
     }
     
-    int pop()
+    int top()
     {
-        int a=q1.front();
-        q1.pop();
-        return a;
-        
-    }
-    
-    int top() 
-    {
-        int a=q1.front();
-        return a;
+        return q.front();
         
     }
     
     bool empty()
     {
-        if(q1.empty()==0)
-        {
-            return false;
-        }
-        else
-        {
-            return true;
-        }
+        return (q.empty()==0)?false:true;
         
     }
 };
