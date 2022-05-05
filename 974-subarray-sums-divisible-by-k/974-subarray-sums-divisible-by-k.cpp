7e4fa04a061c3a8b1c8f3c506a35952033de156c
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-05 19:17:40 +0530

    Time: 105 ms (22.12%), Space: 32 MB (17.85%) - LeetHub

--- 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.cpp
+++ 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.cpp
@@ -5,16 +5,28 @@ public:
         int n=nums.size();
         map<int,int>a;
         int sum=0,rem=0,result=0;
-        a[0]=1;
+        a[0]=1; //..... zero is the case mandatory taken into consideration as 
+        //..... the elements whoose sum is going to be divisible by 7 is done.....
+        
         for(int i=0;i<n;i++)
         {
             sum+=nums[i];
             rem=sum%k;
+            
+            //case 2:- ess vle mae agar kisi ne negative number eg-> -1...-2..-3..-4....-5...
+           //     then adding the k into it gives the positive number eg:-
+            //  kn-y => kn-y+k-k => k(n-1)+ (k-y)
+            //   -4  => -4+7     => 3 
+                    
             if(rem<0)
             {
                 rem=rem+k;
             }
-            if(a.find(rem) != a.end()){
+            
+           // if the element is not present it gives -1 which is equal to end() so it will
+           //     run all the time except the case when it is present more then one time
+            if(a.find(rem) != a.end())
+            {
                 //add it to the result
                 //this means we have frequency of remainder greater than one 
                 result += a[rem];
