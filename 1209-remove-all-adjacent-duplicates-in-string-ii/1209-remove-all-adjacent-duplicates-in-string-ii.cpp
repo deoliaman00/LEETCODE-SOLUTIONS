7a8abac99747b7bb3353f48211a8cdb2a6df2c06
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-06 19:28:13 +0530

    Time: 23 ms (72.43%), Space: 10.9 MB (36.83%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string removeDuplicates(string s, int k) 
+    {
+        auto j = 0;
+  vector<int> cnt(s.size(), 1);
+  for (auto i = 0; i < s.size(); ++i, ++j) {
+    s[j] = s[i];
+    if (j > 0 && s[j] == s[j - 1]) cnt[j] = cnt[j - 1] + 1;
+    else cnt[j] = 1;
+    if (cnt[j] == k) j -= k;
+  }
+  return s.substr(0, j);
+        
+    }
+};
