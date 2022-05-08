2c7e632283563d4b1b72ec8bf7da88714e1d3e73
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-08 15:44:45 +0530

    Time: 317 ms (54.44%), Space: 107.8 MB (42.21%) - LeetHub

--- 381-insert-delete-getrandom-o1-duplicates-allowed/381-insert-delete-getrandom-o1-duplicates-allowed.cpp
+++ 381-insert-delete-getrandom-o1-duplicates-allowed/381-insert-delete-getrandom-o1-duplicates-allowed.cpp
@@ -1,54 +1,111 @@
+// class RandomizedCollection {
+// public:
+//     unordered_map<int,set<int>> um;
+//     vector<int> v;
+//     int size;
+//     RandomizedCollection() {
+//         v.resize(100005,-1);
+//         size=0;
+//     }
+    
+//     bool insert(int val) {
+//         bool flag;
+//         if(um.count(val))
+//             flag=false;
+//         else
+//             flag=true;
+//         um[val].insert(size);
+//         v[size] = val;
+//         size++;
+//         return flag;
+//     }
+    
+//     bool remove(int val) {
+//         if(um.count(val) == 0)
+//             return false;
+//         auto it = um[val].end();
+//         --it;
+//         int x = *it;
+//         um[val].erase(it);
+//         if(um[val].size() == 0)
+//             um.erase(val);
+//         if(size == 1 || x == size-1)
+//         {
+//             size--;
+//             v[x] = -1;
+//             return true;
+//         }
+//         v[x] = v[size-1];
+//         it = um[v[size-1]].end();
+//         --it;
+//         um[v[size-1]].erase(it);
+//         um[v[size-1]].insert(x);
+//         v[size-1]=-1;
+//         size--;
+//         return true;
+//     }
+    
+//     int getRandom() {
+//         return v[rand()%size];
+//     }
+// };
+    
 class RandomizedCollection {
 public:
-    unordered_map<int,set<int>> um;
-    vector<int> v;
-    int size;
+    unordered_map<int,unordered_set<int>>mp;
+    vector<int>v;
+    
+    
     RandomizedCollection() {
-        v.resize(100005,-1);
-        size=0;
+       mp.clear();   
+       v.clear();
     }
     
+    
     bool insert(int val) {
-        bool flag;
-        if(um.count(val))
-            flag=false;
-        else
-            flag=true;
-        um[val].insert(size);
-        v[size] = val;
-        size++;
+ 
+        bool flag=0;
+        if(!mp[val].size())
+            flag=1;
+
+        mp[val].insert(v.size());
+        v.push_back(val);
+        
         return flag;
     }
     
+    
+    
+    
     bool remove(int val) {
-        if(um.count(val) == 0)
-            return false;
-        auto it = um[val].end();
-        --it;
-        int x = *it;
-        um[val].erase(it);
-        if(um[val].size() == 0)
-            um.erase(val);
-        if(size == 1 || x == size-1)
-        {
-            size--;
-            v[x] = -1;
-            return true;
-        }
-        v[x] = v[size-1];
-        it = um[v[size-1]].end();
-        --it;
-        um[v[size-1]].erase(it);
-        um[v[size-1]].insert(x);
-        v[size-1]=-1;
-        size--;
-        return true;
+        
+        if(!mp[val].size())
+            return 0;
+        
+        int k=*mp[val].begin();
+        mp[val].erase(k);
+        
+     if(mp[v[v.size()-1]].size())
+     {  
+         int l=v[v.size()-1];
+         mp[l].erase(v.size()-1);
+         mp[l].insert(k);
+         swap(v[k],v[v.size()-1]);
+     }
+        
+        v.pop_back();
+        return 1;
     }
     
+    
+    
+    
+    
     int getRandom() {
-        return v[rand()%size];
+      return v[rand()%v.size()];
     }
 };
+
 
 /**
  * Your RandomizedCollection object will be instantiated and called as such:
