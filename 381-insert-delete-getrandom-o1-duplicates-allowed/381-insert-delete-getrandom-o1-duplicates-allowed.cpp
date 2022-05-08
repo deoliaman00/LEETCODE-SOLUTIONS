20bbeca00c8b5bdc6f83c5f89924ef6b1f013b8c
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-08 15:41:39 +0530

    Time: 335 ms (48.65%), Space: 119.5 MB (5.02%) - LeetHub

---
+++ 381-insert-delete-getrandom-o1-duplicates-allowed/381-insert-delete-getrandom-o1-duplicates-allowed.cpp
@@ -0,0 +1,59 @@
+class RandomizedCollection {
+public:
+    unordered_map<int,set<int>> um;
+    vector<int> v;
+    int size;
+    RandomizedCollection() {
+        v.resize(100005,-1);
+        size=0;
+    }
+    
+    bool insert(int val) {
+        bool flag;
+        if(um.count(val))
+            flag=false;
+        else
+            flag=true;
+        um[val].insert(size);
+        v[size] = val;
+        size++;
+        return flag;
+    }
+    
+    bool remove(int val) {
+        if(um.count(val) == 0)
+            return false;
+        auto it = um[val].end();
+        --it;
+        int x = *it;
+        um[val].erase(it);
+        if(um[val].size() == 0)
+            um.erase(val);
+        if(size == 1 || x == size-1)
+        {
+            size--;
+            v[x] = -1;
+            return true;
+        }
+        v[x] = v[size-1];
+        it = um[v[size-1]].end();
+        --it;
+        um[v[size-1]].erase(it);
+        um[v[size-1]].insert(x);
+        v[size-1]=-1;
+        size--;
+        return true;
+    }
+    
+    int getRandom() {
+        return v[rand()%size];
+    }
+};
+
+/**
+ * Your RandomizedCollection object will be instantiated and called as such:
+ * RandomizedCollection* obj = new RandomizedCollection();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
