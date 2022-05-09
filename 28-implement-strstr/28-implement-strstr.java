a72b35d424371679189448dc1dc98f9c880a3dcd
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-09 06:22:44 +0530

    Time: 1 ms (74.18%), Space: 42 MB (65.82%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,19 +1,4 @@
-// class Solution {
-// // public int strStr(String haystack, String needle) {
-// // int hay_len=haystack.length();
-// // int needle_len=needle.length();
-// // if(needle_len==0)
-// // return 0;
-// // if(needle_len>hay_len)
-// // return -1;
-// // for(int i=0;i<=hay_len-needle_len;i++)
-// // {
-// // if(needle.equals(haystack.substring(i,i+needle_len)))
-// // return i;
-// // }
-// // return -1;
-// // }
-// // }
+
 
 class Solution
 {
