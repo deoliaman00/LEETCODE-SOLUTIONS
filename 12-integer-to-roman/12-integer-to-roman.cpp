03067c55f0de70119ad7fd6cb815fadc58ad3496
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-09 18:40:06 +0530

    Time: 30 ms (11.68%), Space: 9.5 MB (18.04%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -1,7 +1,3 @@
-class Solution {
-public:
-    string intToRoman(int n)
-    {
 //         string res;
 //         string sym[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 //         int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
@@ -16,18 +12,51 @@ public:
 //         }
         
 //         return res;
-        vector<pair<int, string>> m = {{1000, "M"},{900, "CM"} , {500, "D"},{400, "CD"} , {100, "C"} , {90, "XC"}, {50, "L"}, {40, "XL"},{10, "X"}, {9, "IX"},{5, "V"} , {4, "IV"},{1, "I"} };
-        string ans = "";
-        while(n > 0){
-            for(auto x : m){
-                if(n >= x.first){
-                    ans += x.second;
-                    n = n - x.first;
+
+// vector<pair<int, string>> m = {{1000, "M"},{900, "CM"}
+//                                        , {500, "D"},{400, "CD"}
+//                                        , {100, "C"} , {90, "XC"},
+//                                        {50, "L"}, {40, "XL"},
+//                                        {10, "X"}, {9, "IX"},
+//                                        {5, "V"} , {4, "IV"},
+//                                        {1, "I"} };
+//         string ans = "";// XLVIII
+//         while(n > 0)
+//         {
+//             for(auto x : m)
+//             {
+//                 if(n >= x.first)
+//                 {
+//                     ans += x.second;
+//                     n = n - x.first;
+//                     break;
+//                 }
+//             }
+//         }
+        
+//         return ans;
+class Solution 
+{
+public:
+    string intToRoman(int n)
+    {
+        vector<pair<string,int>>check{{"M",1000},{"CM",900},{"D",500},{"CD",400},{"C",100},{"XC",90},{"L",50},{"XL",40},{"X",10},{"IX",9},{"V",5},{"IV",4},{"I",1}};
+        string ans="";
+        
+        while(n>0)
+        {
+            for(auto i:check)
+            {
+                if(n>=i.second)
+                {
+                    ans+= i.first;
+                    n-=i.second;
                     break;
                 }
             }
         }
         
         return ans;
+        
     }
 };
