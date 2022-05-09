c905f27f403f9e3e6be031db941fac8b94405cf5
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-09 06:22:23 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (83.17%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,16 +1,42 @@
-class Solution {
-public int strStr(String haystack, String needle) {
-int hay_len=haystack.length();
-int needle_len=needle.length();
-if(needle_len==0)
-return 0;
-if(needle_len>hay_len)
-return -1;
-for(int i=0;i<=hay_len-needle_len;i++)
+// class Solution {
+// // public int strStr(String haystack, String needle) {
+// // int hay_len=haystack.length();
+// // int needle_len=needle.length();
+// // if(needle_len==0)
+// // return 0;
+// // if(needle_len>hay_len)
+// // return -1;
+// // for(int i=0;i<=hay_len-needle_len;i++)
+// // {
+// // if(needle.equals(haystack.substring(i,i+needle_len)))
+// // return i;
+// // }
+// // return -1;
+// // }
+// // }
+
+class Solution
 {
-if(needle.equals(haystack.substring(i,i+needle_len)))
-return i;
-}
-return -1;
-}
+    public int strStr(String haystack,String needle)
+    {
+        int l1=haystack.length();
+         int l2=needle.length();
+        int n1=l1-l2; // only till here search will be implemented
+        if(l2>l1)
+        {
+            return -1;
+        }
+        if(l2==0)
+        {
+            return 0;
+        }
+        for(int i=0;i<=n1;i++)
+        {
+            if(needle.equals(haystack.substring(i,i+l2)))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
