651dbb16a3ecb9a5043f3764b370d05484ebab21
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-11 00:13:45 +0530

    Time: 59 ms (38.34%), Space: 20.5 MB (57.40%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -2,16 +2,24 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs)
     {
-         unordered_map<string, vector<string>> mp;
-        for (string s : strs) {
-            string t = s; 
-            sort(t.begin(), t.end());
-            mp[t].push_back(s);
+        //.. we will use unordered map for 0(1) complexities
+        unordered_map<string,vector<string>>judging;
+        // juding map will see who gets same anagram
+        
+        for(auto i:strs)
+        {
+            string f=i;
+            sort(f.begin(),f.end());
+            judging[f].push_back(i);
         }
-        vector<vector<string>> anagrams;
-        for (auto p : mp) { 
-            anagrams.push_back(p.second);
+         vector<vector<string>> group;
+        for(auto i:judging)
+        {
+            group.push_back(i.second);
         }
-        return anagrams;
+        
+        return group;
+     
+        
     }
 };
