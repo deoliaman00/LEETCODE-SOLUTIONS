43c2bb493ecf10a9b6410d83de0cbd904cecfb04
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-10 23:52:50 +0530

    Time: 6 ms (16.27%), Space: 6.4 MB (58.80%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     
  vector<vector<int>>res;
-    void dfs(vector<int>& arr, vector<int>& prefix, int sum, int idx, int k, int n)
+    void dfs(vector<int>& A, vector<int>& prefix, int sum, int idx, int k, int n)
     {
         if(prefix.size()==k and sum==n) {res.push_back(prefix);return;}
         if(sum>n) return;
-        for(int i=idx ; i<arr.size() ; i++)
+        for(int i=idx ; i<A.size() ; i++)
         {
-            prefix.push_back(arr[i]);
-            dfs(arr,prefix,sum+arr[i],i+1,k,n);
+            prefix.push_back(A[i]);
+            dfs(A,prefix,sum+A[i],i+1,k,n);
             prefix.pop_back();
         }
     }
     
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int>arr,prefix;
-        for(int i=1 ; i<=9 ; i++) arr.push_back(i);
-        dfs(arr,prefix,0,0,k,n);
+        vector<int>A,prefix;
+        for(int i=1 ; i<=9 ; i++) A.push_back(i);
+        dfs(A,prefix,0,0,k,n);
         return res;
     }
 };
