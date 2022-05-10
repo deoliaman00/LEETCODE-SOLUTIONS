806be69edcb43ac806a1b8a5d164f5a85536f20a
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-10 18:16:37 +0530

    Time: 9 ms (46.40%), Space: 10.7 MB (28.82%) - LeetHub

--- 71-simplify-path/71-simplify-path.cpp
+++ 71-simplify-path/71-simplify-path.cpp
@@ -3,51 +3,52 @@ public:
     string simplifyPath(string path)
     {
         //    /home/ => /home      /../  => /      /home//foo/    =>   /home/foo
-        stack<string>a;
-        string rem;
+        stack<string>a; // a stack will be used to enter the fields
+        string rem; // a extra string for managing final ans
          
-        int n=path.size();
+        int n=path.size(); // size of string
+        
         for(int i=0;i<n;i++)
         {
-            if(path[i]=='/')
+            if(path[i]=='/') // if the first element starts with /
             {
                 continue;
             }
-            string temp;
+            string temp; // temp used to add the result to rem
             
            
             
             //int n=path.length();
-        while(i<n && path[i]!='/')
+        while(i<n && path[i]!='/') // eg-> /../teiop////opop//  => ".." and "teiop" and "opop"     
         {
-            temp+=path[i];   
+            temp+=path[i];    //  i will stop if the path encounters / 
             i++;
                 
         }
-            if(temp==".")
+            if(temp==".") // the case at which it is neglected 
             {
                 continue;
             }
-            else if(temp=="..")
+            else if(temp=="..") // the case at which we will pop the previous entry and move further
             {
-                if(a.empty()==0)
+                if(a.empty()==0) // if stack empty is equals false then pop the previous entry 
                 {
                     a.pop();
                 }
             }
             else
             {
-                a.push(temp);
+                a.push(temp); // if temp is string "aman" b/w ////aman/
             }
         
     }
         
-        while(a.empty()==0)
+        while(a.empty()==0) // jabtk stack is not empty pop kro 
         {
-            rem="/"+a.top()+rem;
+            rem="/"+a.top()+rem; // ist try-> /foo  iind try->  /true/foo
             a.pop();
         }
-        if(rem.size()==0)
+        if(rem.size()==0) // for case /../ 
         {
             return "/";
         }
