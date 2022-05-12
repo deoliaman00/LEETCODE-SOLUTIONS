727b10c91b9a3ad826a19eb6a788c3ac4451a893
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-12 19:11:50 +0530

    Time: 13 ms (52.46%), Space: 9 MB (71.29%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,29 +1,64 @@
+// solution 1
+// class Solution {
+//     private:
+//     set<vector<int>>ans;
+    
+// public:
+//     void permii(vector<int>nums,int indx, vector<vector<int>>&permuteUniq)
+//     {
+//         if(indx>=nums.size() && ans.find(nums)==ans.end()) // find should be equal to -1
+//         {
+//             ans.insert(nums);
+//          permuteUniq.push_back(nums);   
+//             return;
+//         }
+        
+//         for(int i=indx;i<nums.size();i++)
+//         {
+//             swap(nums[i],nums[indx]); // 0 0  1 0  2 0 
+//             permii(nums,indx+1,permuteUniq);
+//             swap(nums[i],nums[indx]);
+            
+//         }
+        
+//     }
+//     vector<vector<int>> permuteUnique(vector<int>& nums)
+//     {
+       
+//         vector<vector<int>> perm;
+       
+//         permii(nums,0,perm);
+//         return perm;
+//     }
+// };
+
+
+
 class Solution {
     private:
-    set<vector<int>>ans;
-    
+     
 public:
     void permii(vector<int>nums,int indx, vector<vector<int>>&permuteUniq)
     {
-        if(indx>=nums.size() && ans.find(nums)==ans.end()) // find should be equal to -1
+        if(indx==nums.size()-1) // find should be equal to -1
         {
-            ans.insert(nums);
+           
          permuteUniq.push_back(nums);   
             return;
         }
         
         for(int i=indx;i<nums.size();i++)
         {
+            if (indx!= i && nums[i] == nums[indx]) continue;
             swap(nums[i],nums[indx]); // 0 0  1 0  2 0 
             permii(nums,indx+1,permuteUniq);
-            swap(nums[i],nums[indx]);
-            
+                       
         }
         
     }
     vector<vector<int>> permuteUnique(vector<int>& nums)
     {
-       
+       sort(nums.begin(),nums.end());
         vector<vector<int>> perm;
        
         permii(nums,0,perm);
