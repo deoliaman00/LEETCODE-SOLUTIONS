af015f50716a15913400215929cf27679b38add1
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-14 23:30:53 +0530

    Time: 3 ms (57.20%), Space: 6.3 MB (42.80%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -2,93 +2,32 @@ class Solution {
 public:
     bool isHappy(int n)
     {
-//         set<int>check;
-//         int val,i=0;
-//         bool flag=true;
-        
-//         while(1)
-//         {
-//             val=n;
-//             while(n)
-//             {
-//                 val=n%10;
-//                 val+=i*i;
-//                 n=n/10;
-//             }
-//             if(val==1)
-//             {
-//                 return true;
-//             }
-//             else if(check.find(val)!=check.end())
-//             {
-//                 return false;
-//             }
-//             check.insert(val);
-//             n=val;
-//         }
-//         return false;
-//     //    return flag;
-        
-        
-       //USING SET
-
-
-    
-           set<int> myset;
-        int val;
-        int index;
+        set<int>check;
+        int val,i=0;
+        bool flag=true;
         
         while(1)
         {
-            val = 0;
+            val=0;
             while(n)
             {
-                index = n%10;
-                val += index*index;
-                n /=10;
+                i=n%10;
+                val+=i*i;
+                n=n/10;
             }
             if(val==1)
+            {
                 return true;
-            else if(myset.find(val)!=myset.end())
+            }
+            else if(check.find(val)!=check.end())
+            {
                 return false;
-            
-            myset.insert(val);
-            n = val;
+            }
+            check.insert(val);
+            n=val;
         }
         return false;
-
-
-
-// //USING TRIAL & ERROR
-
-// class Solution {
-// public:
-//     bool isHappy(int n) {
-    
-//         int counter = 6;
-//         int val;
-//         int index;
-        
-//         while(counter)
-//         {
-//             val = 0;
-//             while(n)
-//             {
-//                 index = n%10;
-//                 val += index*index;
-//                 n /=10;
-//             }
-//             if(val==1)
-//                 return true;
-            
-//             n = val;
-//             counter -= 1;
-//         }
-//         return false;
-//     }
-// }; 
-       
-
+    //    return flag;
         
     }
 };
