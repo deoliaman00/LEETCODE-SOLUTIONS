7d5d1ca8b1dcae75b96f92af783e2463cf2b2eac
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-15 00:44:30 +0530

    Time: 4 ms (42.30%), Space: 5.9 MB (64.07%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-    int digitSqSum(int n){
-        int sum=0;
-        while(n>0){
-            sum=sum+((n%10)*(n%10));
+    int digitSqSum(int n)
+    {
+       int sum=0;
+        while(n>0)
+        {
+            sum=sum+ ((n%10)*(n%10));
             n=n/10;
         }
         return sum;
     }
     
-    bool isHappy(int n) {        
+    bool isHappy(int n)
+    {        
         int temp=n;
         while(1)
         {
@@ -21,4 +24,16 @@ public:
         }
         
     }
+    
+    /*
+    That is because the sum of squares of digits always ends in 1 or 89.
+For example,
+
+44 → 32 → 13 → 10 → 1 → 1
+85 → 89 → 145 → 42 → 20 → 4 → 16 → 37 → 58 → 89
+Therefore any chain that arrives at 1 or 89 will become stuck in an endless loop.
+
+*/
+    
+    
 };
