5b97cae6f2a8e61cff4c3b1a791bfbbfec0413e4
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-14 14:03:59 +0530

    Time: 3 ms (70.75%), Space: 6.4 MB (54.15%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
-    string addBinary(string a, string b) 
+    string addBinary(string a, string b)
     {
-        string result;
-        int i=a.length()-1,j=b.length()-1;
+        int m=a.size(),n=b.size();
+        int i=m-1,j=n-1;
         int carry=0;
+        string s="";
         while(i>=0 || j>=0)
         {
             int sum=carry;
             if(i>=0)
             {
-                sum+= a[i]-'0';
+                sum+=a[i]-'0';
                 i--;
             }
             if(j>=0)
             {
-                sum+= b[j]-'0';
+                sum+=b[j]-'0';
                 j--;
             }
+            
+            s+=(sum%2+'0');
             carry=sum/2;
-            result+= to_string(sum%2);
+           
         }
         
-       if(carry!=0)
-       {
-           result+= to_string(carry);
-       }
+        if(carry!=0)
+        {
+            s+=(carry+'0');
+        }
         
-        reverse(result.begin(),result.end());
-    
-        return result;
+        reverse(s.begin(),s.end());
+        
+        return s;
+        
     }
 };
