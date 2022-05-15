d87e11b338bc2f771bc47988212c059eb5eb9bfc
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-15 21:36:08 +0530

    Time: 3 ms (59.36%), Space: 6 MB (34.35%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int reverse(int x)
+    {
+        // approach2 
+        int digit=0,ans=0;
+        while(x)
+        {
+            digit=x%10;
+            if((ans>INT_MAX/10)|| (ans<INT_MIN/10))    
+            {
+               return 0;
+            }
+             ans= ans*10+digit;
+            x=x/10;      
+        }
+        
+        return ans;
+        
+    }
+};
