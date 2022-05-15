b930e0c95c3a5dee5686edb2bfd6774b778e97ed
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-15 22:30:51 +0530

    Time: 8 ms (7.01%), Space: 5.9 MB (70.47%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -2,18 +2,41 @@ class Solution {
 public:
     bool isPowerOfTwo(int n)
     {
-        for(int i=0;i<=30;i++)
+        int a=1;
+        
+         for(int i=0;i<=30;i++)
         {
-            if(n==pow(2,i))
+             
+            if(n==a)
             {
                 return true;
             }
-            if(n<pow(2,i))
-            {
-                return false;
-            }
-            
+             if(a<INT_MAX/2)
+             {
+                a=a*2;
+             }
+             
         }
         return false;
     }
-};
+    };
+// app2
+// class Solution {
+// public:
+//     bool isPowerOfTwo(int n)
+//     {
+//         for(int i=0;i<=30;i++)
+//         {
+//             if(n==pow(2,i))
+//             {
+//                 return true;
+//             }
+//             if(n<pow(2,i))
+//             {
+//                 return false;
+//             }
+            
+//         }
+//         return false;
+//     }
+// };
