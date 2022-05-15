0cce4349129b61341446e0abcede16b16c5ee3a8
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-15 11:10:00 +0530

    Time: 12 ms (85.71%), Space: 12.5 MB (42.86%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
@@ -1,63 +1,63 @@
-class Solution {
-public:
-    vector<string> removeAnagrams(vector<string>& words) 
-    {
-        int n=words.size();
-        vector<string>ans;
-        ans.push_back(words[0]);
-                      
-        for(int i=1;i<n;i++)
-        {
-            string a=words[i];
-            sort(a.begin(),a.end());
-            string b=ans.back();
-            sort(b.begin(),b.end());
-            if(a!=b)
-            {
-                ans.push_back(words[i]);
-            }
-            
-        }
-        
-        return ans;
-        
-    }
-};
-
-// approach2
 // class Solution {
 // public:
 //     vector<string> removeAnagrams(vector<string>& words) 
 //     {
-//         vector<string>res;
-        
-//         set<string>st;
-        
-        
-//         for(int i=0;i<words.size();i++)
+//         int n=words.size();
+//         vector<string>ans;
+//         ans.push_back(words[0]);
+                      
+//         for(int i=1;i<n;i++)
 //         {
-//             string temp=words[i];
-            
-//             sort(temp.begin(),temp.end());
-            
-//             if(st.find(temp)!=st.end())
+//             string a=words[i];
+//             sort(a.begin(),a.end());
+//             string b=ans.back();
+//             sort(b.begin(),b.end());
+//             if(a!=b)
 //             {
-//                 continue;
+//                 ans.push_back(words[i]);
 //             }
             
-//             else
-//             {
-//                 res.push_back(words[i]);
-                
-//                 st.erase(st.begin(),st.end());
-                
-//                 st.insert(temp);
-//             }
 //         }
         
-        
-        
-//         return res;
+//         return ans;
         
 //     }
 // };
+
+//approach2
+class Solution {
+public:
+    vector<string> removeAnagrams(vector<string>& words) 
+    {
+        vector<string>res;
+        
+        set<string>st;
+        
+        
+        for(int i=0;i<words.size();i++)
+        {
+            string temp=words[i];
+            
+            sort(temp.begin(),temp.end());
+            
+            if(st.find(temp)!=st.end())
+            {
+                continue;
+            }
+            
+            else
+            {
+                res.push_back(words[i]);
+                
+                st.erase(st.begin(),st.end());
+                
+                st.insert(temp);
+            }
+        }
+        
+        
+        
+        return res;
+        
+    }
+};
