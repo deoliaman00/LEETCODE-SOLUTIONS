4d28b34f6edd3969b4f60a78ffa8f69a1a16dec8
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-15 10:30:52 +0530

    Time: 10 ms (85.71%), Space: 12.5 MB (42.86%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
@@ -4,18 +4,29 @@ public:
     {
 vector<string>res;
         set<string>st;
-        for(int i=0;i<words.size();i++){
+        for(int i=0;i<words.size();i++)
+        {
             string temp=words[i];
+            
             sort(temp.begin(),temp.end());
-            if(st.count(temp)){
+            
+            if(st.find(temp)!=st.end())
+            {
                 continue;
             }
-            else{
+            
+            else
+            {
                 res.push_back(words[i]);
+                
                 st.erase(st.begin(),st.end());
+                
                 st.insert(temp);
             }
         }
+        
+        
+        
         return res;
         
     }
