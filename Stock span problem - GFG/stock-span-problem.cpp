ace1436243e2c79edab0461940e918fb88393816
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-17 08:51:27 +0530

    Added solution - LeetHub

--- Stock span problem - GFG/stock-span-problem.cpp
+++ Stock span problem - GFG/stock-span-problem.cpp
@@ -10,34 +10,54 @@ class Solution
 {
     public:
     //Function to calculate the span of stockâ€™s price for all n days.
-    vector <int> calculateSpan(int price[], int n)
+    vector <int> calculateSpan(int a[], int n)
     {
-            stack<pair<int,int>> s;
-      vector<int> vec;
-      vector<int> span(n);
-      for(int i=0;i<n;i++)
-      {
-          if(s.empty())
-          vec.push_back(-1);
-          else
-          {
-              if(s.top().first>price[i])
-              vec.push_back(s.top().second);
-              else
-              {
-                  while(!s.empty()&&s.top().first<=price[i])
-                  s.pop();
-                  if(s.empty())
-                  vec.push_back(-1);
-                  else
-                  vec.push_back(s.top().second);
-              }
-          }
-          s.push({price[i],i});
-      }
-      for(int i=0;i<n;i++)
-      span[i]=i-vec[i];
-      return span;
+   
+    
+   //int n=a.size();
+    stack<pair<int,int>>s;
+    vector<int>ans1;
+    vector<int>ans(n);
+        
+     
+        for(int i=0;i<n;i++)
+        {
+            if(s.empty())
+            {
+                ans1.push_back(-1);
+            }
+            else
+            {
+               if(s.top().first>a[i])
+                {
+                ans1.push_back(s.top().second);
+                }
+               else{
+               
+                while(!s.empty()&& s.top().first<=a[i])
+                {
+                    s.pop();
+                }
+                if(s.empty())
+                {
+                    ans1.push_back(-1);
+                }
+                else
+                {
+                    ans1.push_back(s.top().second);
+                }
+                   
+               }
+            }
+             s.push({a[i],i});
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+           // ans.push_back(i-ans1[i]);
+            ans[i]=i-ans1[i];
+        }
+    return ans;
         
     }
 };
