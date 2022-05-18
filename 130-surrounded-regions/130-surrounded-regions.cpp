86216f4c3f1e61f79ded10bb9b23ec0f71431a63
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-19 04:45:10 +0530

    Time: 26 ms (20.75%), Space: 10 MB (52.67%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -1,71 +1,48 @@
-class Solution
-{
-    bool seen;
+class Solution {
+public:      
+void solve(vector<vector<char>>& board) {
+    int n=board.size();
+    int m=board[0].size();        
     
-    void mark(vector<vector<char>>& board,int i,int j,int r,int c)
+    // calling dfs function only for border cells
+    for(int i=0;i<n;i++)
     {
-        if(i<0 || i>r-1 || j<0 || j>c-1)
-            return;
-        if(board[i][j]=='X')
-            return;
-        
-        board[i][j] = 'X';
-        mark(board,i-1,j,r,c);
-        mark(board,i+1,j,r,c);
-        mark(board,i,j-1,r,c);
-        mark(board,i,j+1,r,c);       
-    }
-    
-    void dfs(vector<vector<char>>& board,int i,int j,int r,int c,vector<vector<bool>>& visited)
-    {
-        if(i<0 || i>r-1 || j<0 || j>c-1)
-            return;
-        
-        if(board[i][j]=='X' || visited[i][j])
-            return;
-        if(i<=0 || i>=r-1 || j<=0 || j>=c-1)
-            seen = true;
-        
-        visited[i][j] = true;
-        dfs(board,i-1,j,r,c,visited);
-        dfs(board,i+1,j,r,c,visited);
-        dfs(board,i,j-1,r,c,visited);
-        dfs(board,i,j+1,r,c,visited);
-    }
-    
-public:
-    void solve(vector<vector<char>>& board)
-    {
-        /*
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        
-        
-        if(r<=1)
-            return;
-        
-        if(c<=1)
-            return;
-        */
-        
-        int r=board.size();
-        int c=board[0].size();
-        vector<vector<bool>> visited(r,vector<bool>(c,false));
-        
-        for(int i=1;i<r-1;++i)
+        for(int j=0;j<m;j++)
         {
-            for(int j=1;j<c-1;++j)
-            {
-                if(board[i][j]=='O' && !visited[i][j])
-                {
-                    seen = false;
-                    dfs(board,i,j,r,c,visited);
-                    if(seen==false)
-                        mark(board,i,j,r,c);
-                    seen = true;
-                }
-            }
+            if((i==0 || j==0 || i==n-1 || j==m-1) && board[i][j]=='O')
+                dfs(board,i,j);
         }
-        
     }
+    
+    // changing the values
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            if(board[i][j]=='O')
+                board[i][j]='X';
+            if(board[i][j]=='#')
+                board[i][j]='O';
+        }            
+     }  
+  }
+
+void dfs(vector<vector<char>>& board,int i,int j)
+{
+    int n=board.size();
+    int m=board[0].size();
+    
+    // base cases
+    if(i<0 || j<0 || i>=n || j>=m || board[i][j]!='O')
+        return;
+    
+    // marking the cell as visited by assigning '#'
+    board[i][j]='#';
+    
+    // calling dfs function for valid directions
+     dfs(board,i-1,j);
+     dfs(board,i+1,j);
+     dfs(board,i,j-1);
+     dfs(board,i,j+1);
+    }       
 };
