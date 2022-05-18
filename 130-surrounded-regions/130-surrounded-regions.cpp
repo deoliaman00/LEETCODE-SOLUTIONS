6c8a593c59e4b41aa971beae0092f44ded40f18d
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-18 21:09:18 +0530

    Time: 24 ms (24.68%), Space: 15.5 MB (10.75%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -1,40 +1,7 @@
-// class Solution {
-// public:
-//     void dfs(vector<vector<char>>& board,int i,int j,int m,int n)
-//     {
-//         if(i<0 || j<0 || i==m-1 || j==n-1 || i>=m || i<=n)
-//         {
-//             return;
-//         }
-//         board[i][j]='X';
-//         dfs(board,i+1,j,m,n);
-//         dfs(board,i,j+1,m,n);
-//         dfs(board,i-1,j,m,n);
-//         dfs(board,i,j-1,m,n);
-        
-//     }
-//     void solve(vector<vector<char>>& board)
-//     {
-//         int m=board.size();
-//         int n=board[0].size();
-        
-//         for(int i=0;i<m;i++)
-//         {
-//             for(int j=0;j<n;j++)
-//             {
-//                 if(board[i][j]=='O')
-//                 {
-//                     dfs(board,i,j,m,n);
-//                 }
-//             }
-//         }
-        
-//     }
-// };
-
 class Solution
 {
     bool seen;
+    
     void mark(vector<vector<char>>& board,int i,int j,int r,int c)
     {
         if(i<0 || i>r-1 || j<0 || j>c-1)
@@ -48,6 +15,7 @@ class Solution
         mark(board,i,j-1,r,c);
         mark(board,i,j+1,r,c);       
     }
+    
     void dfs(vector<vector<char>>& board,int i,int j,int r,int c,vector<vector<bool>>& visited)
     {
         if(i<0 || i>r-1 || j<0 || j>c-1)
@@ -64,17 +32,24 @@ class Solution
         dfs(board,i,j-1,r,c,visited);
         dfs(board,i,j+1,r,c,visited);
     }
+    
 public:
-    void solve(vector<vector<char>>& board) {
+    void solve(vector<vector<char>>& board)
+    {
+        /*
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         
-        int r=board.size();
+        
         if(r<=1)
             return;
-        int c=board[0].size();
+        
         if(c<=1)
             return;
+        */
+        
+        int r=board.size();
+        int c=board[0].size();
         vector<vector<bool>> visited(r,vector<bool>(c,false));
         
         for(int i=1;i<r-1;++i)
@@ -91,5 +66,6 @@ public:
                 }
             }
         }
+        
     }
 };
