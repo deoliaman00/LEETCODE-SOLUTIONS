a89907e817d3eb56b163357cf30b54dad705efed
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-24 19:41:26 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (34.26%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& mm)
+    void rotate(vector<vector<int>>& matrix)
     {
-        int m=mm.size();
-        int n=mm[0].size();
+        int m=matrix.size();
+        int n=matrix[0].size();
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<i;j++)
             {
-                swap(mm[i][j],mm[j][i]);
+                swap(matrix[i][j],matrix[j][i]);
             }
-            
         }
         
         for(int i=0;i<m;i++)
         {
-           
-                reverse(mm[i].begin(),mm[i].end());
+            reverse(matrix[i].begin(),matrix[i].end());
         }
-            
         
         
         
