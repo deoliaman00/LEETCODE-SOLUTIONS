e065fd8a7761578bf736fa9f2e404fee1a1a5073
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-29 10:31:00 +0530

    Time: 5 ms (100.00%), Space: 6.2 MB (33.33%) - LeetHub

--- 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.cpp
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
-    int rearrangeCharacters(string s, string target) 
+    int rearrangeCharacters(string s, string target)
     {
-       int ans = 1e9;
-        unordered_map<char,int> mp1,mp2;
-        for(auto it:s) mp1[it]++;
-        for(auto it:target) mp2[it]++;
-        for(auto it:mp2){
-            ans = min(ans,mp1[it.first] / it.second);
-        } 
+        int ans=INT_MAX;
+        unordered_map<char,int>mp1;
+        unordered_map<char,int>mp2;        
+        
+        for(auto i:s)
+        {
+         mp1[i]++;   
+        }
+        
+        for(auto i:target)
+        {
+            mp2[i]++;
+        }
+        
+        for(auto k:mp2)
+        {
+            ans=min(ans,mp1[k.first]/k.second);
+        }
         return ans;
-        
-        
     }
 };
