f6ff7914591fb2b4f06f9c3186e549cb246d0707
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-05-31 18:22:40 +0530

    Time: 912 ms (5.62%), Space: 24.1 MB (22.83%) - LeetHub

--- 934-shortest-bridge/934-shortest-bridge.cpp
+++ 934-shortest-bridge/934-shortest-bridge.cpp
@@ -1,73 +1,63 @@
 class Solution {
 public:
-    
-    queue<pair<int, int>> q; // queue
-    
-    void dfs(vector<vector<int>>& grid, int i, int j) { // for marking the 1st island visited
+    void flood_fill(vector<vector<int>>& grid,int i,int j,vector<pair<int,int>>&vec)
+    {
+        int m=grid.size();
+        int n=grid[0].size();
         
-        if (i<0 or i>=grid.size() or j<0 or j>=grid[0].size() or grid[i][j] == 2 or grid[i][j] == 0)
+        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]==0)
             return;
         
-        grid[i][j] = 2; // marking them visited
-        q.push({i,j}); // adding them to queue
+        grid[i][j]=0;
+        vec.push_back({i,j});
+        flood_fill(grid,i+1,j,vec);
+        flood_fill(grid,i,j+1,vec);
+        flood_fill(grid,i-1,j,vec);
+        flood_fill(grid,i,j-1,vec);
+        return;
         
-        dfs(grid, i+1, j);
-        dfs(grid, i-1, j);
-        dfs(grid, i, j+1);
-        dfs(grid, i, j-1);
     }
-    
-    int bfs(vector<vector<int>>& grid){ // finding the nearest bridge between visited island and unvisited island
-        
-        int d = 0; // dist
-        int mindist = INT_MAX; // to store minimum distance
-        
-        vector<vector<int>> dir = {{1,0}, {0,1}, {-1,0}, {0,-1}};
-        
-        while (!q.empty()){
-            int n = q.size();
-            
-            while(n-- > 0){
-                auto a = q.front();
-                q.pop();
-
-                for (int h=0; h<4; h++) {
-                    int x = dir[h][0] + a.first;
-                    int y = dir[h][1] + a.second;
+    int shortestBridge(vector<vector<int>>& grid)
+    {
+        vector<pair<int,int>>pp1;// for first island
+        vector<pair<int,int>>pp2; // for second island
+        int count=0;
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<grid[0].size();j++)
+            {
+                
+                if(grid[i][j]==1)
+                {
+                    count++;
+                    if(count==1)
+                    {
+                        flood_fill(grid,i,j,pp1); // this is the first island     
+                    }
+                     if(count==2)
+                    {
+                        flood_fill(grid,i,j,pp2); // this is the second island     
+                    }
                     
-                    if (x>=0 and x<grid.size() and y>=0 and y<grid[0].size() and grid[x][y] == 1) { // if the neighbor is 1, then check if its minimum distance
-                        mindist = min(mindist, d);
-                    }
-
-                    else if (x>=0 and x<grid.size() and y>=0 and y<grid[0].size() and grid[x][y] == 0) { // if the neighbor is 0, then mark it visited and add it to queue
-                        q.push({x, y});
-                        grid[x][y] = 2;
-                    }
                 }
             }
-            d++; // increasing each level by distance + 1
         }
         
-        return mindist; // returning min dist found till end
-    }
-    
-    int shortestBridge(vector<vector<int>>& grid) {
+        int mini=INT_MAX;
         
-        bool flag = false;
-        
-        for (int i=0; i<grid.size(); i++){
-            for (int j=0; j<grid[0].size(); j++){
-                if (grid[i][j] == 1 and !flag){ // found 1st '1' in the matrix
-                    dfs(grid, i, j); // dfs for marking the whole island visited
-                    q.push({i,j});
-                    flag = true;
-                    break;
+        for(int i=0;i<pp1.size();i++)
+        {
+            for(int j=0;j<pp2.size();j++)
+            {
+                    int dist=abs(pp1[i].first-pp2[j].first)+abs(pp1[i].second-pp2[j].second)-1;
+                
+                if(mini>dist)
+                {
+                    mini=dist;
                 }
             }
-            if (flag)
-                break;
         }
-        
-        return bfs(grid); // bfs for getting min dist and returning it
+        return mini;
     }
 };
+        
