66140968ae3048649f555ad475271c4d68860a74
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-07-23 15:13:35 +0530

    Time: 8 ms (67.06%), Space: 9 MB (22.53%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    
+   // METHOD 1 AND 2 RECURSION AND MEMOIZATION
+        
     int find(int i,int j,int n,vector<vector<int>>&grid,vector<vector<int>>&dp)
     {
         if(i==n-1)return grid[i][j];
@@ -14,8 +17,31 @@ public:
     int minimumTotal(vector<vector<int>>&grid)
     {
         int n=grid.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        int ans=find(0,0,n,grid,dp);
-        return ans;
+        vector<vector<int>>dp(n,vector<int>(n,0));
+    //     int ans=find(0,0,n,grid,dp);
+    //     return ans;
+        
+       // int i=n-1;
+        
+        for(int j=0;j<n;j++)
+        {
+            dp[n-1][j]=grid[n-1][j];
+        }
+        
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                int down=grid[i][j]+dp[i+1][j];
+                 int diag=grid[i][j]+dp[i+1][j+1];
+        
+                dp[i][j]=min(down,diag);
+            }
+        }
+        
+        
+        return dp[0][0];
     }
+        
+        
 };
