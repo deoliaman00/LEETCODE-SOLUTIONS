68032b61f2ab511708288f0c16382aaac5dae077
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-07-29 20:41:12 +0530

    Time: 259 ms (12.07%), Space: 45.2 MB (23.53%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,21 +1,72 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int n) {
-        // creating the base dp array, with first value set to 0
-        int dp[++n];
-        dp[0] = 0;
-        // more convenient to have the coins sorted
-        sort(begin(coins), end(coins));
-        // populating our dp array
-        for (int i = 1; i < n; i++) {
-            // setting dp[0] base value to 1, 0 for all the rest
-            dp[i] = INT_MAX;
-            for (int c: coins) {
-                if (i - c < 0) break;
-                // if it was a previously not reached cell, we do not add use it
-                if (dp[i - c] != INT_MAX) dp[i] = min(dp[i], 1 + dp[i - c]);
+    /*
+    int func(int indx,int target,vector<int>coins,vector<vector<int>>&dp)
+    {
+        if(dp[indx][target]!=-1)return dp[indx][target];
+        if(indx==0)
+        {
+            if(target%coins[0]==0)
+                return target/coins[0];
+            else
+                return 1e9;
+        }
+        
+        
+        int ntaken=0+func(indx-1,target,coins,dp);
+        int take=1e9;
+        if(coins[indx]<=target)
+        {
+            take=1+func(indx,target-coins[indx],coins,dp);
+        }
+        
+        return dp[indx][target]=min(ntaken,take);
+    }
+    */
+    
+    int coinChange(vector<int>& coins, int amount)
+    {
+        int n=coins.size();
+       // vector<vector<int>>dp(n,vector<int>(amount+1,-1));----> memoization array of dp
+        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        
+        //int res= func(n-1,amount,coins,dp);
+        //if(res>=1e9)return -1;
+        //else
+          //  return res;
+
+        for(int i=0;i<=amount;i++)
+        {
+            if(i%coins[0]==0)
+            {
+                dp[0][i]=i/coins[0];
+            }else{
+                dp[0][i]=1e9;
             }
         }
-        return dp[--n] == INT_MAX ? -1 : dp[n];
+        for(int indx=1;indx<n;indx++)
+        {
+            for(int tar=0;tar<=amount;tar++)
+            {
+                    
+                int ntaken=0+dp[indx-1][tar];
+                int take=1e9;
+                if(coins[indx]<=tar)
+                {
+                     take=1+dp[indx][tar-coins[indx]];
+                }
+        
+            dp[indx][tar]=min(ntaken,take);
+            }
+        }
+    
+    int a=dp[n-1][amount];
+        if(a>=1e9)
+        {
+            return -1;
+        }else
+        {
+            return dp[n-1][amount];
+        }
     }
 };
