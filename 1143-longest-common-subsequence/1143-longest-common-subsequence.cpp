06593113ff0560f23f7beb09e7c70e5d07ca8491
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-07-31 17:44:09 +0530

    Time: 29 ms (64.52%), Space: 13.2 MB (21.91%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -12,11 +12,28 @@ public:
     
     int longestCommonSubsequence(string text1, string text2)
     {
-        int ind1=text1.size()-1;
-        int ind2=text2.size()-1;
-        vector<vector<int>>dp(ind1+1,vector<int>(ind2+1,-1));
-        int ans=func(ind1,ind2,text1,text2,dp);
-        return ans;
+        int indx1=text1.size();
+        int indy2=text2.size();
+      //  vector<vector<int>>dp(ind1+1,vector<int>(ind2+1,-1));
+      //  int ans=func(ind1,ind2,text1,text2,dp);
+       // return ans;
+        
+        vector<vector<int>>dp(indx1+1,vector<int>(indy2+1,0));
+        
+        for(int i=0;i<=indx1;i++)dp[i][0]=0;
+        for(int i=0;i<=indy2;i++)dp[0][i]=0;
+        for(int ind1=1;ind1<=indx1;ind1++)
+        {
+            for(int ind2=1;ind2<=indy2;ind2++)
+            {
+                if(text1[ind1-1]==text2[ind2-1]) dp[ind1][ind2]=1+dp[ind1-1][ind2-1];
+                else 
+                {
+                    dp[ind1][ind2]=0+max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+                }
+            }
+        }
+        return dp[indx1][indy2];
         
     }
 };
