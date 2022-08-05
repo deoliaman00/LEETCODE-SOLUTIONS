78f84aa8f2e0994d2db417b42df5f943c6792055
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-08-05 12:18:07 +0530

    Time: 4 ms (23.97%), Space: 5.8 MB (90.91%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.cpp
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -2,10 +2,22 @@ class Solution {
 public:
     int mirrorReflection(int p, int q)
     {
-         while (p % 2 == 0 && q % 2 == 0){ 
-            p/=2;
-            q/=2;
+        /*
+        if p is even and q is odd => return 2
+        if p is odd and q is even => return 0
+        if p is odd and q is odd => return 1
+          */
+        while(p%2==0 && q%2==0)
+        {
+            p=p/2;
+            q=q/2;
         }
-        return 1 - p % 2 + q % 2;
+        
+        if(p%2==0 && q%2!=0)return 2;
+        if(p%2!=0 && q%2==0)return 0;
+        if(p%2!=0 && q%2!=0)return 1;
+        
+        
+        return -1;
     }
 };
