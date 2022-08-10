8eaa723a044f869f4560d38bc0933ac78ad6aa1a
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-08-10 18:58:43 +0530

    Time: 85 ms (74.77%), Space: 9.3 MB (54.77%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -1,18 +1,43 @@
+
 class Solution {
 public:
-    int numFactoredBinaryTrees(vector<int>& A)
+    int numFactoredBinaryTrees(vector<int>& arr)
     {
-        long res = 0, mod = pow(10, 9) + 7;
-        sort(A.begin(), A.end());
-        unordered_map<int, long> dp;
-        for (int i = 0; i < A.size(); ++i) {
-            dp[A[i]] = 1;
-            for (int j = 0; j < i; ++j)
-                if (A[i] % A[j] == 0)
-                    dp[A[i]] = (dp[A[i]] + dp[A[j]] * dp[A[i] / A[j]]) % mod;
-            res = (res + dp[A[i]]) % mod;
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+        map<int,long long int>mp;
+        for(int i=0;i<n;i++)
+        {
+            mp.insert({arr[i],1});
         }
-        return res;
+        
+        for(int indx=1;indx<n;indx++)
+        {
+            long long int count=0;
+            auto it3=mp.find(arr[indx]);
+            for(int prev=0;prev<indx;prev++)
+            {
+                if(arr[indx]%arr[prev]==0)
+                {
+                    // eg=> arr[indx]=> 10 so ====> 10%2==0 count of 2 and count of 5 is added to 10 map
+                    auto it1=mp.find(arr[prev]);
+                    auto it=mp.find(arr[indx]/arr[prev]);
+                    if(it!=mp.end()){
+                    count+=(it1->second)*(it->second);  
+                    }
+                }
+                
+            }
+            it3->second+=count;
+        }
+        
+        long long  int alo=0;
+        for(auto mppr:mp)
+        {
+            alo+=mppr.second;
+        }
+        
+        return alo%(1000000007);
         
     }
 };
