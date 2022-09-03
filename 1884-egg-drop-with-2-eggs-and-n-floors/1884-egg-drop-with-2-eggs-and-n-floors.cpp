20645813df7b858d43674ebf40e61ed5a08913f3
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-09-03 23:29:18 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (19.84%) - LeetHub

---
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+     int f(int K, int N) {
+          vector<vector<int>> dp(K+1, vector<int>(N+1,0));
+        int m = 0;
+        while (dp[K][m] < N) {
+            m++;
+            for (int k = 1; k <= K; k++)
+                dp[k][m] = dp[k][m - 1] + dp[k - 1][m - 1] + 1;
+        }
+        return m;
+    }
+    int twoEggDrop(int n) 
+    {
+        int eggs=2;
+        int floors=n;
+        int ways=f(eggs,floors);
+        return ways;
+    }
+};
