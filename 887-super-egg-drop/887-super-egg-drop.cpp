d7e372c19f0bc793754663c05079a14b6e13b1c0
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-09-04 01:45:40 +0530

    Time: 111 ms (57.87%), Space: 29.4 MB (55.33%) - LeetHub

--- 887-super-egg-drop/887-super-egg-drop.cpp
+++ 887-super-egg-drop/887-super-egg-drop.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-      int solve(int e,int f,vector<vector<int>>&t){
-        if(f==0||f==1)
-            return f;
-        if(e==1)
-            return f;
-        if(t[e][f]!=-1)
-            return t[e][f];
-        int mn=INT_MAX;
-        int low=0,high=f,temp=0;
-        while(low<=high){
-            int mid=(low+high)/2;
-            int down=solve(e-1,mid-1,t); //check down floor
-            int up=solve(e,f-mid,t);//check upper floor
-             temp=1+max(down,up);//max bcz need worst case
-            if(down<up)//for worst case changing value
-                low=mid+1;
-            else
-                high=mid-1;
-            
-            
-            mn=min(mn,temp);
-        }
-        return t[e][f]=mn;
-    }
-    int superEggDrop(int k, int n) {
-        vector<vector<int>>t(k+1,vector<int>(n+1,-1));
-        //memset(t,-1,sizeof(t));
-        return solve(k,n,t);
-    }
+int find(int k,int n,vector<vector<int>> &dp)
+	{   if(n==0||n==1) return n;  
+		if(k==1) return n;           
+		if(dp[k][n]!=-1) return dp[k][n];
+		int ans=1000000,l=1,h=n,temp=0;
+		while(l<=h)
+		{
+			int mid=(l+h)/2;
+			int left=find(k-1,mid-1,dp);   
+			int right=find(k,n-mid,dp) ;   
+			temp=1+max(left,right);           
+			if(left<right){                  
+			  l=mid+1;                       
+			}
+			else                             
+			{    
+				h=mid-1;
+			}
+			ans=min(ans,temp);               
+		}
+
+
+		return dp[k][n]=ans;
+
+	}
+	int superEggDrop(int K, int N) {
+		vector<vector<int>> dp(K+1,vector<int> (N+1,-1));
+		return find(K,N,dp);
+
+	}
+
+
 };
