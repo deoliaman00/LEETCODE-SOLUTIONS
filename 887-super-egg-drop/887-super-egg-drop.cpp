a2a1d5dc92f82266e71619f226c2bd557caa531d
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-09-04 01:09:42 +0530

    Time: 142 ms (42.22%), Space: 29.6 MB (40.14%) - LeetHub

--- 887-super-egg-drop/887-super-egg-drop.cpp
+++ 887-super-egg-drop/887-super-egg-drop.cpp
@@ -1,13 +1,32 @@
 class Solution {
 public:
-    int superEggDrop(int K, int N) {
-        vector<vector<int>> dp(K+1, vector<int>(N+1,0));
-        int m = 0;
-        while (dp[K][m] < N) {
-            m++;
-            for (int k = 1; k <= K; k++)
-                dp[k][m] = dp[k][m - 1] + dp[k - 1][m - 1] + 1;
+      int solve(int e,int f,vector<vector<int>>&t){
+        if(f==0||f==1)
+            return f;
+        if(e==1)
+            return f;
+        if(t[e][f]!=-1)
+            return t[e][f];
+        int mn=INT_MAX;
+        int low=0,high=f,temp=0;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int down=solve(e-1,mid-1,t); //check down floor
+            int up=solve(e,f-mid,t);//check upper floor
+             temp=1+max(down,up);//max bcz need worst case
+            if(down<up)//for worst case changing value
+                low=mid+1;
+            else
+                high=mid-1;
+            
+            
+            mn=min(mn,temp);
         }
-        return m;
+        return t[e][f]=mn;
+    }
+    int superEggDrop(int k, int n) {
+        vector<vector<int>>t(k+1,vector<int>(n+1,-1));
+        //memset(t,-1,sizeof(t));
+        return solve(k,n,t);
     }
 };
