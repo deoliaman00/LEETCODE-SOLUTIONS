2cbfb97e37eeb2eb415c684f582b7da399c53048
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-09-05 21:30:53 +0530

    Time: 7 ms (70.88%), Space: 13 MB (71.73%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,29 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-    struct Node {
-        TreeNode* n;
-        int x, y;
-    };
 public:
-    vector<vector<int>> verticalTraversal(TreeNode* root) {
-        vector<vector<int>> ans;
-        map<int, map<int, vector<int>>> mp;
-        queue<Node*> q;
-        q.push(new Node{root, 0, 0});
-        while(!q.empty()) {
-            Node* node = q.front();
-            q.pop();
-            if(node->n->left) q.push(new Node{node->n->left, node->x - 1, node->y + 1});
-            if(node->n->right) q.push(new Node{node->n->right, node->x + 1, node->y + 1});
-            mp[node->x][node->y].push_back(node->n->val);
-        }
-        for(pair<int, map<int, vector<int>>> p1 : mp) {
-            ans.push_back({});
-            for(pair<int, vector<int>> p2 : p1.second) {
-                vector<int> v = p2.second;
-                sort(v.begin(), v.end());
-                ans.back().insert(ans.back().end(), v.begin(), v.end());
-            }
-        }
-        return ans;
+    void f(map<int,vector<pair<int,int>>> &mp,TreeNode *root,int right,int left)
+{
+    if(root==NULL)
+    {
+        return;
     }
+    mp[left].push_back({right,root->val});
+    f(mp,root->left,right+1,left-1);
+    f(mp,root->right,right+1,left+1);
+}
+    
+    vector<vector<int>> verticalTraversal(TreeNode* root)
+    {
+        map<int,vector<pair<int,int>>> mp;
+        vector<vector<int>> ver;
+        f(mp,root,0,0);
+        for(auto i:mp)
+        {
+            sort(i.second.begin(),i.second.end());
+            vector<int>temp;
+            for(auto pp:i.second)
+            {
+                temp.push_back(pp.second);
+            }
+            ver.push_back(temp);
+        }
+        
+        return ver;
+    }
+    
 };
