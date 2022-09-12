b9bfacd4e5a789c84943763a0a2a738479ec20db
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-09-12 12:18:33 -0700

    Time: 8 ms (72.03%), Space: 10.5 MB (66.86%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.cpp
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -2,18 +2,36 @@ class Solution {
 public:
     int bagOfTokensScore(vector<int>& tokens, int power)
     {
-         sort(tokens.begin(),tokens.end());
-        int i, j, n;
-        i = 0; j = n = tokens.size();
-        while(i < j) {
-            if(power >= tokens[i]) 
-                power -= tokens[i++];
-            else if(i - (n - j) && j > i + 1)
-                power += tokens[--j];
-            else
-                break;
-        }
-        return i - (n - j);
+        sort(tokens.begin(),tokens.end());
+        int i=0,j,n;
+        j=n=tokens.size();
+        j--;
+        int maxi=0;
+        int score=0;
         
+        while(i<=j)
+        {
+           if(power>=tokens[i])
+           {
+               score++;
+               power-=tokens[i];
+              i++;
+               maxi=max(maxi,score);
+           }
+            else if(score>=1)
+            {
+                power+=tokens[j];
+                j--;
+               // i++;
+                score--;
+            }
+            else
+            {
+                break;
+            }
+          //  i++;
+        }
+        
+        return maxi;
     }
 };
