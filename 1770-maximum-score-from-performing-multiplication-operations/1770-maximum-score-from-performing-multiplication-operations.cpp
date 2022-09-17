befbb91145b856d3078e2acba8f53c2f453c7242
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-09-17 06:11:26 -0700

    Time: 428 ms (90.96%), Space: 120 MB (67.48%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    
-    vector<vector<int>> dp;
-    
-    int solve(int i, int n, int j, vector<int> &nums, vector<int> &M){
-        
-        if (j == M.size()) return 0;
-        if (dp[i][j] != INT_MIN) return dp[i][j];
-        
-        // Left Side
-        int left = solve(i + 1, n, j + 1, nums, M) + (nums[i] * M[j]);
-        
-        // Right Side
-        int right = solve(i, n, j + 1, nums, M) + (nums[(n - 1) - (j - i)] * M[j]);
-        
-        return dp[i][j] = max(left, right);
+    vector<vector<int>>dp;
+    int f(int indx,int left,int right,vector<int>& nums, vector<int>& mus)
+    {
+        if(indx==mus.size())return 0;
+        if(dp[indx][left]!=INT_MIN)
+        {
+            return dp[indx][left];
+        }
+        int l=nums[left]*mus[indx]+f(indx+1,left+1,right,nums,mus);
+        int r=nums[right]*mus[indx]+f(indx+1,left,right-1,nums,mus);
+        return dp[indx][left]=max(l,r);
     }
-    
-    int maximumScore(vector<int>& nums, vector<int>& M) {
-        int n = nums.size(), m = M.size();
-        dp.resize(m + 1, vector<int>(m + 1, INT_MIN));
-        return solve(0, n, 0, nums, M);
+    int maximumScore(vector<int>& nums, vector<int>& mus) 
+    {
+        int n=nums.size()-1;
+        int m=mus.size();
+        dp.assign(m+1,vector<int>(m+1,INT_MIN));
+        return f(0,0,n,nums,mus);
+        
     }
 };
-
-   
