1d2c87ab615f6eb56a88624b245712185234df4a
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-09-19 19:57:01 -0700

    Time: 620 ms (20.83%), Space: 132.2 MB (75.48%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -25,10 +25,30 @@ public:
        // int turn=1;
         int i=0;
         int n=stone.size();
-        vector<int>dp(50001,-1);
-        int ans=f(i,n,stone,dp);
-        if(ans>0)return "Alice";
-        if(ans==0)return "Tie";
+        vector<int>dp(n+1,0);
+       // int ans=f(i,n,stone,dp);
+       
+        
+        for(int indx=n-1;indx>=0;indx--)
+        {
+             int ans=INT_MIN;
+            if(indx<n)ans=max(ans,stone[indx]-dp[indx+1]);
+            if(indx+1<n)
+            {
+                ans=max(ans,stone[indx]+stone[indx+1]-dp[indx+2]);
+            }
+            if(indx+2<n)
+            {
+                ans=max(ans,stone[indx]+stone[indx+1]+stone[indx+2]-dp[indx+3]);
+            }
+        dp[indx]=ans;
+        }
+        
+        
+        
+        
+        if(dp[0]>0)return "Alice";
+        if(dp[0]==0)return "Tie";
         return "Bob";
     }
 };
