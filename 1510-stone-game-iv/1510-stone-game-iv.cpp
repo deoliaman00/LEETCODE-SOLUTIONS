4a1ab63e828ce606dbea72cb503755ef70fc7be9
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-09-21 06:34:35 -0700

    Time: 124 ms (55.29%), Space: 13.7 MB (25.81%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,28 +1,5 @@
 class Solution {
 public:
-    vector<int>dp;
-    bool winnerSquareGame(int n) {
-        
-        //0->False
-        //1->true
-        dp.resize(n+1,-1);
-       return fnc(n,dp);
-    }
-    
-    int fnc(int n,vector<int>&dp)
-    {
-         if(n==0)return 0;
-         if(dp[n]!=-1)return dp[n];
-        for(int i=1;i*i<=n;i++)
-        {   //n = 6
-            if(fnc(n-i*i,dp)==0)
-                return dp[n]=1;
-        }
-        
-        return dp[n]=0;
-        
-    }};
-    /*
     int f(int n,vector<int>&dp)
     {
         if(n==0)return 0;//b too......
@@ -42,10 +19,10 @@ public:
     }
     bool winnerSquareGame(int n)
     {
-        vector<int>dp(n+1,false);
-        int ans=f(n,dp);
-        if(ans==1)return true;
-        else return false;
+        vector<int>dp(n+1,-1);
+        return f(n,dp);
+       
         
     }
-    */
+    
+};
