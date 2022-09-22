64f3971bbf0a74416d57e1169841403f0b4f0269
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-09-22 03:33:30 -0700

    Time: 469 ms (16.35%), Space: 127.6 MB (10.06%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.cpp
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -10,9 +10,9 @@ public:
         for (auto &x : stones)
             a[x % 3]++;
         bool flip = (a[0] % 2 > 0);
-        if (!a[1])
+        if (a[1]==0)
             return (a[2] < 3 ? BOB : flip);
-        if (!a[2])
+        if (a[2]==0)
             return (a[1] < 3 ? BOB : flip);
         if (abs(a[1] - a[2]) > 2)
             return ALICE;
