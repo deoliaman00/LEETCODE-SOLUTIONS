1c98aa51b4dcc7df74fb125c0e735e8410eec3a2
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-09-22 21:41:45 -0700

    Time: 508 ms (6.91%), Space: 152.5 MB (5.08%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,47 +1,56 @@
-class Solution {
+class Solution 
+{
 public:
-    vector<int> dirs = {-1,0,1};
-    int dp[100][100][100];
-    int backtrack(int i,int j1,int j2, vector<vector<int>> &grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        if(i<0 || i>=n || j1>=m || j2>=m || j1<0 || j2<0) {
-            return 0;
-        }
+    vector<int>indx={-1,0,1};
+    int f(int i1,int j1,int j2,vector<vector<int>>& grid,vector<vector<vector<int>>>&dp)
+    {
+        int m=grid.size();
+        int n=grid[i1].size();
         
-        if(i==n-1) {
-            if(j1==j2) { // common case
-                return grid[i][j1];
-            } else {
-                return grid[i][j1] + grid[i][j2];
+        if(i1>=m || i1<0 || j1>=n || j2>=n || j1<0 || j2<0)return 0;
+        if(dp[i1][j1][j2]!=-1)return dp[i1][j1][j2];
+        if(i1==m-1)
+        {
+            if(j1==j2)
+            {
+                return dp[i1][j1][j2]=grid[i1][j1];
+            }
+            else
+            {
+                return dp[i1][j1][j2]=grid[i1][j1]+grid[i1][j2];
             }
         }
         
-        if(dp[i][j1][j2]!=-1) {
-            return dp[i][j1][j2];
-        }
-            
-        
-        int maxi = INT_MIN;
-        for(auto d1:dirs) {
-            for(auto d2:dirs) {
-                if(j1==j2) { // common case
-                    maxi = max(maxi,grid[i][j1]+ backtrack(i+1,d1+j1,j2+d2,grid));
-                   
-                   
-                } else {
-                    maxi = max(maxi,grid[i][j1] + grid[i][j2]+ backtrack(i+1,d1+j1,j2+d2,grid));
-                   
-                   
-                }
-                
+        int maxi=INT_MIN;
+        for(auto k:indx)
+        {
+            for(auto d:indx)
+            {
+                    if(j1==j2)
+                    {
+                         maxi=max(maxi,grid[i1][j1]+f(i1+1,j1+k,j2+d,grid,dp));
+                    }
+                    else
+                    {
+                        maxi=max(maxi,grid[i1][j1]+grid[i1][j2]+f(i1+1,j1+k,j2+d,grid,dp));
+                    }
             }
         }
-        return dp[i][j1][j2]=maxi;
+        return dp[i1][j1][j2]=maxi;
     }
-    int cherryPickup(vector<vector<int>>& grid) {
-        int m = grid[0].size();
-        memset(dp,-1,sizeof dp);
-        return backtrack(0,0,m-1,grid);
+    
+    
+    int cherryPickup(vector<vector<int>>& grid) 
+    {
+        // the robot rows keep moving 
+        // the robot colums keep moving 
+        // the robot on right end also keeps its column moving
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<vector<int>>>dp(72,vector<vector<int>>(72,vector<int>(72,-1)));
+        
+        int ans=f(0,0,n-1,grid,dp);
+        return ans;
+        
     }
 };
