ed21e461438e3fb5a4e6691d3beb29e3d67f4019
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-10-08 08:02:50 +0530

    Time: 19 ms (94.92%), Space: 24.9 MB (84.71%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.cpp
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.cpp
@@ -1,28 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void addRow(TreeNode* root,int& val,int depth){
-        if(!root)
-            return;
-        if(depth==1){
-            TreeNode* treeL = new TreeNode(val);
-            TreeNode* treeR = new TreeNode(val);
-            treeL->left = root->left;
-            root->left=treeL;
-            treeR->right = root->right;
-            root->right=treeR;
-            return;
-        }else{
-            addRow(root->left,val,depth-1);
-            addRow(root->right,val,depth-1);
+    void insert(TreeNode* root, int val, int depth,int curD)
+    {
+        // Base condition
+        
+        if(root==NULL)return;
+        
+        if(depth-1==curD)
+        {
+            // done for the left node of the tree 
+            TreeNode *temp=root->left;
+            //TreeNode *j=
+            root->left=new TreeNode(val);
+            root->left->left=temp;
+            
+            // for the right node of the tree
+            temp=root->right;
+           root->right=new TreeNode(val);
+            root->right->right=temp;
+            
+        }
+        else
+        {
+            insert(root->left,val,depth,curD+1);
+             insert(root->right,val,depth,curD+1);
         }
     }
-    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
-        if(depth==1){
-            TreeNode* tree = new TreeNode(val);
-            tree->left=root;
-            return tree;
+    
+    
+    
+    
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) 
+    {
+        if(depth==1)
+        {
+            TreeNode *temp=new TreeNode(val);
+           temp->left=root;
+            return temp;
         }
-        addRow(root,val,depth-1);
-        return root;
+        else
+        {
+         insert(root,val,depth,1);    
+        }
+        return root;        
     }
 };
