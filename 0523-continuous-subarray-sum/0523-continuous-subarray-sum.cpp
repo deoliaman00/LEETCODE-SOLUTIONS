5ef3022e367b6a9f6455c0fcff6ef6968c516abf
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-10-27 07:12:30 +0530

    Time: 621 ms (10.15%), Space: 115.1 MB (19.44%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -7,17 +7,18 @@ public:
         int prefixSum = 0;
         map<int, int>mp; //map to keep a track of remainders
         mp[0] = -1;
-        for (int i = 0; i < n; i++) //at each iteration subarray from (0 to i)
+        for (int i = 0; i < n; i++) 
         {
             prefixSum += nums[i];
             int remainder = prefixSum % k;
-            if (mp.count(remainder)) //if there exist a subarray sum with same remainder
+            if (mp.count(remainder)!=0) 
             { 
                 int len =  i - mp[remainder];
-                if (len == 1) continue; //don't update map here as we want farthest remainder
+               
+                if (len == 1) continue; 
                 if  (len > 1) return true;
             }
-            mp[remainder] = i; //storing the remainder of  current subarray sum  in map
+            mp[remainder] = i;
         }
         return false;
         
