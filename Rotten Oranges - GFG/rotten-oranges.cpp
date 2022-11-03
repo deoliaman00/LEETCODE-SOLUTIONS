ddfd8cc0ff1c6806fbfb3e5905cac8475557faea
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-11-03 07:24:07 +0530

    Added solution - LeetHub

---
+++ Rotten Oranges - GFG/rotten-oranges.cpp
@@ -0,0 +1,99 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution 
+{
+    public:
+    //Function to find minimum time required to rot all oranges. 
+    int orangesRotting(vector<vector<int>>& grid) 
+    {
+        int m=grid.size();
+        int n=grid[0].size();
+        int cnt=0;
+        int sam[m][n];
+        queue<pair<pair<int,int>,int>>pq;
+        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]==2)
+                {
+                    pq.push({{i,j},0});sam[i][j]=2;
+                }
+                else
+                {
+                    sam[i][j]=0;
+                }
+                if(grid[i][j]==1)
+                {
+                    cnt++;
+                  //  sam[i][j]=1;
+                }
+            }
+        }
+        int maxi=0;
+        int arr1[]={-1,0,1,0};
+        int arr2[]={0,1,0,-1};
+        int cnt2=0;
+        while(!pq.empty())
+        {
+            int newI=pq.front().first.first;
+            int newJ=pq.front().first.second;
+            // Both the indexes of the 2,2 has been taken into account now take the time into the next variable
+            int time=pq.front().second;
+            maxi=max(maxi,time);
+            pq.pop();
+            for(int i=0;i<4;i++)
+            {
+                int r=arr1[i]+newI;
+                int c=arr2[i]+newJ;
+                // eg:- 2 wala index jo hai + uske sath jo 4 directions mae sides aari hai unko mark kro check kro
+                if(r>=0 && c>=0 && r<m && c<n && grid[r][c]==1 && sam[r][c]==0)
+                {
+                    pq.push({{r,c},time+1});
+                    sam[r][c]=2;
+                    cnt2++;
+                }
+            }
+        }
+        
+        // for(int i=0;i<m;i++)
+        // {
+        //     for(int j=0;j<n;j++)
+        //     {
+        //         if(grid[i][j]==1)
+        //         {
+        //           cnt1++;
+        //         }
+        //     }
+        // }
+         if(cnt!=cnt2)return -1;
+         return maxi;
+        
+       
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>>grid(n, vector<int>(m, -1));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				cin >> grid[i][j];
+			}
+		}
+		Solution obj;
+		int ans = obj.orangesRotting(grid);
+		cout << ans << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
