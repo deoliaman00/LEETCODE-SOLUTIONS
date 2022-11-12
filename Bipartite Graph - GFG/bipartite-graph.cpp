cecc583cab8b67ba3e5e9d2390322528e84d23c2
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2022-11-12 19:10:26 +0530

    Added solution - LeetHub

--- Bipartite Graph - GFG/bipartite-graph.cpp
+++ Bipartite Graph - GFG/bipartite-graph.cpp
@@ -4,6 +4,46 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
+private:
+    bool dfs(int node,int col,vector<int>adj[],int color[])
+    {
+        color[node]=col;
+        for(auto it:adj[node])
+        {
+            if(color[it]==-1)
+            {
+                //color[it]=!col;
+                if(dfs(it,!col,adj,color)==false)return false;    
+            }
+            else if(color[it]==col){
+                return false;
+            }
+        }
+        return true;
+    }
+public:
+	bool isBipartite(int V, vector<int>adj[])
+	{
+	   int color[V];for(int i=0;i<V;i++)color[i]=-1;
+	   for(int i=0;i<V;i++)
+	   {
+	       if(color[i]==-1)
+	       {
+	        if(dfs(i,0,adj,color)==false)return false;    
+	       }
+	   }
+	   
+	   return true;
+	}
+
+};
+/*
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
     private:
     bool is(int q,int V, vector<int>adj[],int col[])
 	{
@@ -54,6 +94,31 @@ public:
 	}
 
 };
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int V, E;
+		cin >> V >> E;
+		vector<int>adj[V];
+		for(int i = 0; i < E; i++){
+			int u, v;
+			cin >> u >> v;
+			adj[u].push_back(v);
+			adj[v].push_back(u);
+		}
+		Solution obj;
+		bool ans = obj.isBipartite(V, adj);    
+		if(ans)cout << "1\n";
+		else cout << "0\n";  
+	}
+	return 0;
+}
+// } Driver Code Ends
+*/
+
 
 //{ Driver Code Starts.
 int main(){
