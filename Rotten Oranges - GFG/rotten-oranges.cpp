a6fd6e82ea44d912ff40560ba8bc25bab65ed240
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2023-01-11 04:28:39 -0800

    Added solution - LeetHub

--- Rotten Oranges - GFG/rotten-oranges.cpp
+++ Rotten Oranges - GFG/rotten-oranges.cpp
@@ -6,74 +6,60 @@ using namespace std;
 class Solution 
 {
     public:
-    //Function to find minimum time required to rot all oranges. 
-    int orangesRotting(vector<vector<int>>& grid) 
+    
+    int orangesRotting(vector<vector<int>>& grid)
     {
-        int m=grid.size();
-        int n=grid[0].size();
-        int cnt=0;
-        int sam[m][n];
-        queue<pair<pair<int,int>,int>>pq;
-        
-        for(int i=0;i<m;i++)
+        int cnt=0,count=0;
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        // bfs will be implemented
+        queue<pair<pair<int,int>,int>>qt;
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<m;j++)
             {
                 if(grid[i][j]==2)
                 {
-                    pq.push({{i,j},0});sam[i][j]=2;
-                }
-                else
-                {
-                    sam[i][j]=0;
+                    qt.push({{i,j},0});
+                    vis[i][j]=2;
                 }
                 if(grid[i][j]==1)
                 {
                     cnt++;
-                  //  sam[i][j]=1;
                 }
             }
         }
         int maxi=0;
-        int arr1[]={-1,0,1,0};
-        int arr2[]={0,1,0,-1};
-        int cnt2=0;
-        while(!pq.empty())
+        int time=0;
+        int A[]={-1,0,1,0};
+        int B[]={0,1,0,-1};
+        while(!qt.empty())
         {
-            int newI=pq.front().first.first;
-            int newJ=pq.front().first.second;
-            // Both the indexes of the 2,2 has been taken into account now take the time into the next variable
-            int time=pq.front().second;
+            int front=qt.front().first.first;
+            int sec=qt.front().first.second;
+            int time=qt.front().second;
+            qt.pop();
             maxi=max(maxi,time);
-            pq.pop();
             for(int i=0;i<4;i++)
             {
-                int r=arr1[i]+newI;
-                int c=arr2[i]+newJ;
-                // eg:- 2 wala index jo hai + uske sath jo 4 directions mae sides aari hai unko mark kro check kro
-                if(r>=0 && c>=0 && r<m && c<n && grid[r][c]==1 && sam[r][c]==0)
+                int ra=front+A[i];
+                int ca=sec+B[i];
+                if(ra>=0 && ca>=0 && ra<n && ca<m && vis[ra][ca]==0 
+                && grid[ra][ca]==1)
                 {
-                    pq.push({{r,c},time+1});
-                    sam[r][c]=2;
-                    cnt2++;
+                    vis[ra][ca]=1;
+                    qt.push({{ra,ca},time+1});
+                
+                    count++;
                 }
             }
+            
         }
+        if(cnt!=count)return -1;
         
-        // for(int i=0;i<m;i++)
-        // {
-        //     for(int j=0;j<n;j++)
-        //     {
-        //         if(grid[i][j]==1)
-        //         {
-        //           cnt1++;
-        //         }
-        //     }
-        // }
-         if(cnt!=cnt2)return -1;
-         return maxi;
+        return maxi;
         
-       
     }
 };
 
