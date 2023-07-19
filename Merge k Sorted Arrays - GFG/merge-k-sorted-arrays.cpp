0de65ac45dbfd2481faa8a05641ebcebe5dfdf2a
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2023-07-19 23:50:45 +0530

    Added solution - LeetHub

--- Merge k Sorted Arrays - GFG/merge-k-sorted-arrays.cpp
+++ Merge k Sorted Arrays - GFG/merge-k-sorted-arrays.cpp
@@ -14,26 +14,55 @@ for (int i=0; i < size; i++)
 // } Driver Code Ends
 //User function Template for C++
 
+class node
+{
+    public:
+    int value,i,j;
+    node(int data,int index,int aindex)
+    {
+        value=data;
+        i=index;
+        j=aindex;
+    }
+};
+
+struct compare
+{
+    public:
+        bool operator()(node *a1,node *a2)
+        {
+            return a1->value>a2->value;
+        }
+};
 
 class Solution
 {
     public:
     //Function to merge k sorted arrays.
+   
     vector<int> mergeKArrays(vector<vector<int>> arr, int K)
     {
-        vector<int>ans;
-        priority_queue<int,vector<int>,greater<int>>pq;
-        for(auto it:arr)
+        priority_queue<node*,vector<node*>,compare>pq;
+        
+        for(int i=0;i<K;i++)
         {
-            for(auto i:it)
-            {
-                pq.push(i);
-            }
+           node *obj1=new node(arr[i][0],i,0);
+            pq.push(obj1);
         }
+        vector<int>ans;
         while(!pq.empty())
         {
-            ans.push_back(pq.top());
-            pq.pop();
+           node *obj2=pq.top();
+           pq.pop();
+            int val=obj2->value;
+            int indx=obj2->i;
+            int aindx=obj2->j;
+            ans.push_back(val);
+            if(aindx+1<arr[indx].size())
+            {
+                node *obj3=new node(arr[indx][aindx+1],indx,aindx+1);
+                pq.push(obj3);
+            }
         }
         return ans;
     }
