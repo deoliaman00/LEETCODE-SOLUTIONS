512a0783f55264667eb60670448708ced2c81b3a
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2023-07-31 23:08:49 +0530

    Added solution - LeetHub

--- Bipartite Graph - GFG/bipartite-graph.cpp
+++ Bipartite Graph - GFG/bipartite-graph.cpp
@@ -4,121 +4,53 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-private:
-    bool dfs(int node,int col,vector<int>adj[],int color[])
-    {
-        color[node]=col;
-        for(auto it:adj[node])
-        {
-            if(color[it]==-1)
-            {
-                //color[it]=!col;
-                if(dfs(it,!col,adj,color)==false)return false;    
-            }
-            else if(color[it]==col){
-                return false;
-            }
-        }
-        return true;
-    }
 public:
-	bool isBipartite(int V, vector<int>adj[])
-	{
-	   int color[V];for(int i=0;i<V;i++)color[i]=-1;
-	   for(int i=0;i<V;i++)
-	   {
-	       if(color[i]==-1)
-	       {
-	        if(dfs(i,0,adj,color)==false)return false;    
-	       }
-	   }
-	   
-	   return true;
-	}
 
-};
-/*
-//{ Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-class Solution {
-    private:
-    bool is(int q,int V, vector<int>adj[],int col[])
-	{
-	   
-	    col[q]=0;
+    bool bfs(int node,vector<int>adj[],int V,vector<int>&color)
+    {
+       
 	    queue<int>qt;
-	    qt.push(q);
+	    qt.push(node);
+	    
+	    color[node]=0;
 	    while(!qt.empty())
 	    {
 	        int node=qt.front();
 	        qt.pop();
 	        for(auto it:adj[node])
 	        {
-	            if(col[it]==-1){
-	                col[it]=!col[node];
-	                qt.push(it);
-	            }
-	            else if(col[node]==col[it])
+	            if(color[it]==-1)
 	            {
-	                return false;
+	                color[it]=(color[node]==0?1:0);
+	                 qt.push(it);
 	            }
-	            
+	            else if(color[it]==color[node])
+	            {
+	                 return false;
+	            }
+	          
 	        }
 	    }
-	    
 	    return true;
-	    
-	}
-public:
+    }
 	bool isBipartite(int V, vector<int>adj[])
 	{
-	    int col[V];
-	    for(int i=0;i<V;i++){
-	        col[i]=-1;
-	    }
+	    vector<int>color(V,-1);
 	    for(int i=0;i<V;i++)
 	    {
-	        if(col[i]==-1)
+	        if(color[i]==-1)
 	        {
-	            if(is(i,V,adj,col)==false)
+	            if(bfs(i,adj,V,color)==false)
 	            {
 	                return false;
 	            }
 	        }
 	    }
-	    
 	    return true;
+	   
 	}
 
 };
-
-//{ Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int V, E;
-		cin >> V >> E;
-		vector<int>adj[V];
-		for(int i = 0; i < E; i++){
-			int u, v;
-			cin >> u >> v;
-			adj[u].push_back(v);
-			adj[v].push_back(u);
-		}
-		Solution obj;
-		bool ans = obj.isBipartite(V, adj);    
-		if(ans)cout << "1\n";
-		else cout << "0\n";  
-	}
-	return 0;
-}
-// } Driver Code Ends
-*/
-
 
 //{ Driver Code Starts.
 int main(){
