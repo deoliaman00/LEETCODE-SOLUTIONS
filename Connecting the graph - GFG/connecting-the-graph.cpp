33ea33def1d88241f4970c83ec62036599125c7d
AMAN DEOLI <95640925+deoliaman00@users.noreply.github.com>
2023-09-16 12:41:28 +0530

    Added solution - LeetHub

---
+++ Connecting the graph - GFG/connecting-the-graph.cpp
@@ -0,0 +1,156 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class DisjointSet{
+    private:
+    vector<int>rank,parent,size;
+    public:
+    DisjointSet(int n)
+    {
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++)
+        {
+            parent[i] = i;
+            size[i]=1;
+        }
+    }
+    
+    int findPar(int node)
+    {
+        if(node==parent[node])
+        {
+            return node;
+        }
+        return parent[node]=findPar(parent[node]);
+    }
+    void unionBySize(int u,int v)
+    {
+        int u_p=findPar(u);
+        int v_p=findPar(v);
+        if(u_p==v_p)
+        {
+            return;
+        }
+        if(size[u_p]<size[v_p])
+        {
+            parent[u_p]=v_p;
+            size[v_p]+=size[u_p];
+        }
+        else
+        {
+            parent[v_p]=u_p;
+            size[u_p] += size[v_p];
+        }
+    }
+    void unionByRank(int u,int v)
+    {
+        int ultimate_u_parent=findPar(u);
+        int ultimate_v_parent=findPar(v);
+        if(ultimate_v_parent==ultimate_u_parent)
+        {
+            return;
+        }
+        if(rank[ultimate_v_parent]<rank[ultimate_u_parent])
+        {
+            parent[ultimate_v_parent]=ultimate_u_parent;
+        }
+        else if (rank[ultimate_v_parent] > rank[ultimate_u_parent])
+        {
+            parent[ultimate_u_parent]=ultimate_v_parent;
+        }else{
+            parent[ultimate_v_parent]=ultimate_u_parent;
+            rank[ultimate_u_parent]++;
+        }   
+    }
+};
+class Solution {
+  public:
+    int Solve(int n, vector<vector<int>>& edge) 
+    {
+        int extraEdges=0;
+        DisjointSet obj(n);
+        for(auto it:edge)
+        {
+            int u=it[0];
+            int v=it[1];
+            if(obj.findPar(u)==obj.findPar(v))
+            {
+                extraEdges++;
+            }else{
+                obj.unionBySize(u,v);
+            }
+        }
+        
+        // now we need to count the no of connection components
+        
+        int connected=0;
+        for(int i=0;i<n;i++)
+        {
+            if(obj.findPar(i)==i)
+            {
+                connected++;
+            }
+        }
+        connected-=1;
+        if(extraEdges>=connected)return connected;
+        
+        return -1;
+        
+        
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> adj;
+
+        for (int i = 0; i < m; ++i) {
+            vector<int> temp;
+            for (int j = 0; j < 2; ++j) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            adj.push_back(temp);
+        }
+
+        Solution Obj;
+        cout << Obj.Solve(n, adj) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
